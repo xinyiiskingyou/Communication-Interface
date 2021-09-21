83c27844bcade6ad3529224514bbc4bcc7a9f898
Rebecca Hsu <z5361917@ad.unsw.edu.au>
2021-09-21 12:53:00 +1000

    Adding tests for auth_login

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -4,6 +4,13 @@ from src.auth import auth_login_v1
 from src.error import InputError
 from src.other import clear_v1
 
+# Email and password is valid and user is able to successfully login
+def valid_email_password():
+    clear_v1():
+    auth_register_v1('email@unsw.edu.au', 'password', 'name_first', 'name_last')
+    auth_login_v1('email@unsw.edu.au', 'password')
+    pass
+
 # Email tested does not belong to user 
 def test_email_not_belong_user():
     clear_v1()
@@ -18,11 +25,5 @@ def test_incorrect_password():
         auth_register_v1('email@unsw.edu.au', 'password', 'name_first', 'name_last')
         auth_login_v1('email@unsw.edu.au', 'wrong password')
 
-
-# User has not registered with their email
-def test_unregistered_email():
-    clear_v1()
-    with pytest.raises(InputError):
-        auth_login_v1('unregistered.email@unsw.edu.au', 'password')
 
 
