1c1626e440ee9639e0d2ea884efbc75f61e7e8e5
Your Name <email@example.com>
2021-09-21 13:02:40 +1000

    written some tests

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -2,20 +2,30 @@ import pytest
 from src.other import clear_v1
 from src.channels import channels_create_v1
 from src.error import InputError
-def test_create_invalid():
+from src.data_store import data_store
+def test_create_invalid_name():
     clear_v1()
     with pytest.raises(InputError):
         channels_create_v1(123, '', 1)
         channels_create_v1(123, 'abcdefghijklmlaqwertq', 1)
 
-def test_create_valid():
+def test_create_valid_public():
     clear_v1()
     channels_create_v1(123, '1531_CAMEL', 1)
+
+def test_create_valid_private():
+    clear_v1()
+    channels_create_v1(1, 'channel', 0)
 
 def test_create_invalid_id():
     clear_v1()
     channels_create_v1('123', '1531_CAMEL', 1)
+    clear_v1()
+    channels_create_v1('not_a_id', '1531_CAMEL', 1)
 
-def test_create_invalid_public()
+def test_create_invalid_public():
     clear_v1()
     channels_create_v1(123, '1531_CAMEL', -1)
+    clear_v1()
+    channels_create_v1(123, '1531_CAMEL', 100)
+
