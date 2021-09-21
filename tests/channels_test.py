7fa4e67d504a1aceb26797b33630ceb466f94194
Your Name <email@example.com>
2021-09-21 21:50:46 +1000

    added some tests

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -2,11 +2,13 @@ import pytest
 from src.other import clear_v1
 from src.channels import channels_create_v1
 from src.error import InputError
-from src.data_store import data_store
+
 def test_create_invalid_name():
     clear_v1()
     with pytest.raises(InputError):
         channels_create_v1(123, '', 1)
+        channels_create_v1(123, ' ', 1)
+        channels_create_v1(123, '                      ', 1)
         channels_create_v1(123, 'abcdefghijklmlaqwertq', 1)
 
 def test_create_valid_public():
