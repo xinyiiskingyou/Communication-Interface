c23db6743ea03938d162ae55ddbc31ad80a5eb45
Your Name <email@example.com>
2021-09-22 22:18:03 +1000

    wrote tests that checking invalid id, name, is_public

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -1,14 +1,20 @@
 import pytest
-from src.other import clear_v1
+from src.data_store import data_store
 from src.channels import channels_create_v1
 from src.error import InputError
-from src.data_store import data_store
+from src.other import clear_v1
 def test_create_invalid_name():
     clear_v1()
     with pytest.raises(InputError):
         channels_create_v1(123, '', 1)
+    clear_v1()
+    with pytest.raises(InputError):
         channels_create_v1(123, ' ', 1)
+    clear_v1()
+    with pytest.raises(InputError):
         channels_create_v1(123, '                      ', 1)
+    clear_v1()
+    with pytest.raises(InputError):
         channels_create_v1(123, 'abcdefghijklmlaqwertq', 1)
 
 def test_create_valid_public():
@@ -21,13 +27,17 @@ def test_create_valid_private():
 
 def test_create_invalid_id():
     clear_v1()
-    channels_create_v1('123', '1531_CAMEL', 1)
+    with pytest.raises(InputError):
+        channels_create_v1('', '1531_CAMEL', 1)
     clear_v1()
-    channels_create_v1('not_a_id', '1531_CAMEL', 1)
+    with pytest.raises(InputError):
+        channels_create_v1('not_a_id', '1531_CAMEL', 1)
 
 def test_create_invalid_public():
     clear_v1()
-    channels_create_v1(123, '1531_CAMEL', -1)
+    with pytest.raises(InputError):
+        channels_create_v1(123, '1531_CAMEL', -1)
     clear_v1()
-    channels_create_v1(123, '1531_CAMEL', 100)
+    with pytest.raises(InputError):
+        channels_create_v1(123, '1531_CAMEL', 100)
 
