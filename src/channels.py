f5819436b883678f0cbf786828225136b9d25547
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-09-23 23:24:25 +1000

    need to test

--- src/channels.py
+++ src/channels.py
@@ -15,11 +15,20 @@ def channels_listall_v1(auth_user_id):
 
     auth_user_id is the name of the user we connecting from 
     '''
+    from data_store import data_store, initial_object
+    listchannel = []
+    for channels in initial_object['channels']:
+        if channels['auth_user_id'] = auth_user_id;
+            listchannel.append(channels)
+'channels' = listchannel
     return {
         'channels': [
         	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
+        		'channel_id': channels,
+        		'name': name,
+                'is public': is_public,
+                'owner_members': auth_user_id, 
+                'all_members': {auth_user_id}
         	}
         ],
     }
