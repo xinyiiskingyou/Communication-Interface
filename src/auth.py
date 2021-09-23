e752f9a9753f0fffb580ffd1bd515e141ed87277
Sree Varada Raju <sreevraju@gmail.com>
2021-09-23 14:54:08 +1000

    Figured out storage issues. Need to test user_id and handle generation

--- src/auth.py
+++ src/auth.py
@@ -9,16 +9,27 @@ def auth_login_v1(email, password):
 
 def auth_register_v1(email, password, name_first, name_last):
     store = data_store.get()
-    initial_object['users'].append(email)
+
+    # Creating the basic dictionary to be put into the initial_objects list
+    dict_user = {
+        'email': email,
+        'password': password,
+        'name_first': name_first,
+        'name_last': name_last
+    }   
 
     # Error handling
     token = r'\b^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$\b'
     if not (re.search(token, email)):
         raise InputError("This email is of invalid form")
 
-    for user in initial_object['users']:
-        for email in ['user']:
-            if user == email:
+    # If list is empty add new user info (dict) into initial_objects list
+    # else check for duplicates
+    if len(initial_object['users']) == 0:
+        initial_object['users'].append(dict_user)
+    else:
+        for user in initial_object['users']:
+            if user['email'] == email:
                 raise InputError("This email address has already been registered by another user") 
 
     if len(password) < 6:
@@ -30,24 +41,29 @@ def auth_register_v1(email, password, name_first, name_last):
     if len(name_last) not in range(1, 51):
          raise InputError("name_last is not between 1 - 50 characters in length")
          
-    user_id = len(store['users'])
+    # Creating unique user_id and adding to dict_user
+    user_id = len(initial_object['users'])
+    dict_user[user_id] = user_id
 
+    # Creating handle and adding to dict_user
     handle = (name_first + name_last).lower()
     handle = re.sub(r'[^a-z0-9]', '', handle)
     if len(handle) > 20:
         handle = handle[0:20]
+    dict_user[handle] = handle
     
-    initial_object['users'].append(handle)
-    
+    # if list is not empty check for duplicate handles
     number = 0
-    for user in initial_object['users']:
-        for handle in ['user']:
-            if user == handle:
+    if len(initial_object['users']) > 1:
+        for user in initial_object['users']:
+            if user['handle'] == handle:
                 handle = handle + str(number)
                 number += 1
-                if number == 10:
-                    number = 0
-    
+
+    # If list not empty append new dict_user
+    if len(initial_object['users']) > 1:
+        initial_object['users'].append(dict_user)
+
     data_store.set(store)
 
     return {
