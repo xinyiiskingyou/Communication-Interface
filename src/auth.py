f5fdccdd6ff7bde64c87b1662131c55c2663d43b
Sree Varada Raju <sreevraju@gmail.com>
2021-09-23 13:40:05 +1000

    changed the way to store info according to help session but now one test fails

--- src/auth.py
+++ src/auth.py
@@ -1,4 +1,4 @@
-from src.data_store import data_store
+from src.data_store import data_store, initial_object
 from src.error import InputError
 import re
 
@@ -9,15 +9,17 @@ def auth_login_v1(email, password):
 
 def auth_register_v1(email, password, name_first, name_last):
     store = data_store.get()
+    initial_object['users'].append(email)
 
     # Error handling
     token = r'\b^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$\b'
     if not (re.search(token, email)):
         raise InputError("This email is of invalid form")
 
-    for user in store['users']:
-        if user == email:
-           raise InputError("This email address has already been registered by another user") 
+    for user in initial_object['users']:
+        for email in ['user']:
+            if user == email:
+                raise InputError("This email address has already been registered by another user") 
 
     if len(password) < 6:
          raise InputError("This password is less then 6 characters in length")
@@ -29,24 +31,23 @@ def auth_register_v1(email, password, name_first, name_last):
          raise InputError("name_last is not between 1 - 50 characters in length")
          
     user_id = len(store['users'])
-    store['users'].append(email)
 
     handle = (name_first + name_last).lower()
     handle = re.sub(r'[^a-z0-9]', '', handle)
     if len(handle) > 20:
         handle = handle[0:20]
     
-    store['users'].append({email, handle})
+    initial_object['users'].append(handle)
     
     number = 0
-    for user in store['users']:
-        if user == handle:
-            handle = handle + str(number)
-            number += 1
-            if number == 10:
-                number = 0
+    for user in initial_object['users']:
+        for handle in ['user']:
+            if user == handle:
+                handle = handle + str(number)
+                number += 1
+                if number == 10:
+                    number = 0
     
-    store['users'].append({email, handle})
     data_store.set(store)
 
     return {
