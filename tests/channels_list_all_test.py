9efeec7ebfacf63a573753ed1f482e7f383952cf
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-09-24 13:39:53 +1000

    might have finished tests

--- tests/channels_list_all_test.py
+++ tests/channels_list_all_test.py
@@ -1,2 +1,8 @@
 import pytest
 from src.other import clear_v1 
+from src.channels import channels_listall_v1
+from src.data_store import data_store
+
+def test_listall_channels(): 
+    clear_v1()
+    channels_listall_v1(123)
