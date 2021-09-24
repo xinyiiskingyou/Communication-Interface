f118b251c15ce69e13d44df4e670f8485a4311b2
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-09-24 13:23:44 +1000

    finished implementation

--- src/channels.py
+++ src/channels.py
@@ -18,20 +18,18 @@ def channels_listall_v1(auth_user_id):
     from data_store import data_store, initial_object
     listchannel = []
     for channels in initial_object['channels']:
-        if channels['auth_user_id'] = auth_user_id;
-            listchannel.append(channels)
-'channels' = listchannel
-    return {
-        'channels': [
-        	{
-        		'channel_id': channels,
-        		'name': name,
-                'is public': is_public,
-                'owner_members': auth_user_id, 
-                'all_members': {auth_user_id}
-        	}
-        ],
-    }
+        if channels['auth_user_id'] = auth_user_id:
+            listchannel.append({'channel_id' : channel['channel_id'], name: channel['name']})
+    return {'channels': listchannel}
+        # 'channels': [
+        # 	{
+        # 		'channel_id': channels,
+        # 		'name': name,
+        #         'is public': is_public,
+        #         'owner_members': auth_user_id, 
+        #         'all_members': {auth_user_id}
+        # 	}
+        # ],
 
 def channels_create_v1(auth_user_id, name, is_public):
     return {
