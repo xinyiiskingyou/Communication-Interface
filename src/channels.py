40bc4718dd19683f70872c3e7ae7fc306cac4cab
Xinyi Lin <xinyi.lin2@student.unsw.edu.au>
2021-09-26 02:19:15 +1000

    Update channels.py

--- src/channels.py
+++ src/channels.py
@@ -45,26 +45,26 @@ def channels_create_v1(auth_user_id, name, is_public):
         raise InputError('name cannot be blank')
     if is_public not in range(0,2):
         raise InputError('the channel has to be either public or private')
-    if not isinstance(auth_user_id,int):
+    if not isinstance(auth_user_id, int):
         raise InputError('this is an invalid auth user id')
-
-    store = data_store.get()
-    channels = store['channels']
+    if not channels_create_check_valid_user(auth_user_id):
+        raise InputError('this is an invalid auth user id')
+    #channels_create_check_valid_user(auth_user_id)
+    channels = initial_object['channels']
     channel_id = len(channels) + 1
-    new = {
-        'channel_id': channel_id, 
-        'name': name, 
-        'is_public': is_public, 
-        'owner_members': [],
-        'all_members': []
-    }
-    
-    new['all_members'].append({'user_id': auth_user_id})
-    #print('number of channels', channel_id)
-    initial_object['channels'].append(new)
-    data_store.set(store)
+    new = {'channel_id': channel_id, 'name': name, 'is_public': is_public, 'owner_members': auth_user_id, 'all_members': []}
+    new['all_members'].append({'u_id':auth_user_id})
+    channels.append(new)
+    #channels_create_check_valid_user(auth_user_id)
     return {
-        'channel_id': new['channel_id']
+        'channel_id': channel_id,
     }
 
-
+def channels_create_check_valid_user(auth_user_id):
+    '''
+    return type: bool
+    '''
+    for user in initial_object['users']:
+        if user['auth_user_id'] == auth_user_id:
+            return True
+    return False
