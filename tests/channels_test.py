473aaeff3ff69ea8f02c9edc7a05098de8a47970
Your Name <email@example.com>
2021-09-25 20:03:55 +1000

    added auth_register_v1

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -3,26 +3,33 @@ from src.data_store import data_store
 from src.channels import channels_create_v1
 from src.error import InputError
 from src.other import clear_v1
+from src.auth import auth_register_v1
 def test_create_invalid_name():
     clear_v1()
     with pytest.raises(InputError):
-        channels_create_v1(123, '', 1)
+        auth_register_v1('abc@gmail.com', 'password', 'first_name_1', 'last_name_1')
+        channels_create_v1(1, '', 1)
     clear_v1()
     with pytest.raises(InputError):
-        channels_create_v1(123, ' ', 1)
+        auth_register_v1('abc@gmail.com', 'password', 'first_name_1', 'last_name_1')
+        channels_create_v1(1, ' ', 1)
     clear_v1()
     with pytest.raises(InputError):
-        channels_create_v1(123, '                      ', 1)
+        auth_register_v1('abc@gmail.com', 'password', 'first_name_1', 'last_name_1')
+        channels_create_v1(1, '                      ', 1)
     clear_v1()
     with pytest.raises(InputError):
-        channels_create_v1(123, 'abcdefghijklmlaqwertq', 1)
+        auth_register_v1('abc@gmail.com', 'password', 'first_name_1', 'last_name_1')
+        channels_create_v1(1, 'abcdefghijklmlaqwertq', 1)
 
 def test_create_valid_public():
     clear_v1()
-    channels_create_v1(123, '1531_CAMEL', 1)
+    auth_register_v1('abc@gmail.com', 'password', 'first_name_1', 'last_name_1')
+    channels_create_v1(1, '1531_CAMEL', 1)
 
 def test_create_valid_private():
     clear_v1()
+    auth_register_v1('abc@gmail.com', 'password', 'first_name_1', 'last_name_1')
     channels_create_v1(1, 'channel', 0)
 
 def test_create_invalid_id():
@@ -36,8 +43,10 @@ def test_create_invalid_id():
 def test_create_invalid_public():
     clear_v1()
     with pytest.raises(InputError):
-        channels_create_v1(123, '1531_CAMEL', -1)
+        auth_register_v1('abc@gmail.com', 'password', 'first_name_1', 'last_name_1')
+        channels_create_v1(1, '1531_CAMEL', -1)
     clear_v1()
     with pytest.raises(InputError):
-        channels_create_v1(123, '1531_CAMEL', 100)
+        auth_register_v1('abc@gmail.com', 'password', 'first_name_1', 'last_name_1')
+        channels_create_v1(1, '1531_CAMEL', 100)
 
