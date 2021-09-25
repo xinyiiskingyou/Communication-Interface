962c071f3a6a1fd35e8cdcf3c11631db73057aa9
Xinyi Lin <xinyi.lin2@student.unsw.edu.au>
2021-09-26 02:16:51 +1000

    copied

--- src/auth.py
+++ src/auth.py
@@ -17,29 +17,18 @@ def auth_login_v1(email, password):
 
 
 def auth_register_v1(email, password, name_first, name_last):
-    store = data_store.get()
 
-    # Creating the basic dictionary to be put into the initial_objects list
-    dict_user = {
-        'email': email,
-        'password': password,
-        'name_first': name_first,
-        'name_last': name_last
-    }   
+    store = data_store.get()
 
     # Error handling
     token = r'\b^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$\b'
     if not (re.search(token, email)):
         raise InputError("This email is of invalid form")
 
-    # If list is empty add new user info (dict) into initial_objects list
-    # else check for duplicates
-    if len(initial_object['users']) == 0:
-        initial_object['users'].append(dict_user)
-    else:
-        for user in initial_object['users']:
-            if user['email'] == email:
-                raise InputError("This email address has already been registered by another user") 
+    # Check for duplicate emails
+    for user in initial_object['users']:
+        if user['email'] == email:
+            raise InputError("This email address has already been registered by another user") 
 
     if len(password) < 6:
          raise InputError("This password is less then 6 characters in length")
@@ -51,29 +40,41 @@ def auth_register_v1(email, password, name_first, name_last):
          raise InputError("name_last is not between 1 - 50 characters in length")
          
     # Creating unique auth_user_id and adding to dict_user
-    auth_user_id = len(initial_object['users'])
-    dict_user[auth_user_id] = auth_user_id
+    auth_user_id = len(initial_object['users']) + 1
 
     # Creating handle and adding to dict_user
     handle = (name_first + name_last).lower()
     handle = re.sub(r'[^a-z0-9]', '', handle)
     if len(handle) > 20:
         handle = handle[0:20]
-    dict_user[handle] = handle
     
-    # If list is not empty check for duplicate handles in the existing initial_object list. 
+    #Check for duplicate handles
     number = 0
-    if len(initial_object['users']) > 1:
-        for user in initial_object['users']:
-            if user['handle'] == handle:
-                handle = handle + str(number)
-                number += 1
+    for user in initial_object['users']:
+        if user['handle'] == handle:
+            if number == 0: 
+                handle = handle +str(number)
+            elif number in range(1,10):  
+                handle = handle[:-1] + str(number)
+            else:
+                handle = handle[:-2] + str(number)
+
+        number += 1
                 
-        # Then append onto the initial_object list if there are not duplicate handles
-        initial_object['users'].append(dict_user)
+    # Then append dictionary of user email onto initial_objects
+
+    initial_object['users'].append({
+        'email' : email,
+        'password': password,
+        'name_first': name_first,
+        'name_last' : name_last,
+        'auth_user_id' : auth_user_id,
+        'handle' : handle 
+    })
 
     data_store.set(store)
 
     return {
         'auth_user_id': auth_user_id,
     }
+
