5581e02a5a2afa2a9412b81f5a87c526dd46fb80
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-09-26 01:03:11 +1000

    added valid test for both public and private member

--- tests/channel_invite_test.py
+++ tests/channel_invite_test.py
@@ -11,6 +11,7 @@ x_channel = channels_create_v1(x_register['auth_user_id'], 'x', True)
 sally_register = auth_register_v1('email1@gmail.com','comp1531', 'sally','zhou')
 sally_channel = channels_create_v1(sally_register['auth_user_id'], 'sally', False)
 
+y_register = auth_register_v1('email2@gmail.com', 'password', 'y', 'lin')
 def test_channel_invite_input_error():
     clear_v1()
     with pytest.raises(InputError):
@@ -32,7 +33,6 @@ def test_channel_invite_access_error():
     
 def test_valid_invite_public_member():
     
-    y_register = auth_register_v1('email2@gmail.com', 'password', 'y', 'lin')
     # test if public channel member can invite new user
     channel_invite_v1(x_register['auth_user_id'], x_channel['channel_id'], y_register['u_id'])
     y_channel = channels_create_v1(y_register['auth_user_id'], 'y', False)
@@ -56,6 +56,7 @@ def test_valid_invite_private_member():
     z_register = auth_register_v1('email3@gmail.com', 'password', 'z', 'lin')
     channel_invite_v1(sally_register['auth_user_id'], sally_channel['channel_id'], z_register['u_id']) 
     z_channel = channels_create_v1(z_register['auth_user_id'], 'z', True)
+
     assert(channels_list_v2(sally_register['auth_user_id']) == {
 		'channels': [
 			{
