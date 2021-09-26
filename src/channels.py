4fb3aaee4ba1b766a52c255e3802beefd2c41842
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-09-26 22:22:11 +1000

    updated

--- src/channels.py
+++ src/channels.py
@@ -1,7 +1,7 @@
 from src.data_store import data_store, initial_object
 from src.error import InputError
 
-def channels_list_v1(auth_user_id):
+def channels_list_v2(auth_user_id):
     '''
     Provide a list of all channels (and their associated details) 
     that the authorised user is part of.
@@ -19,10 +19,14 @@ def channels_list_v1(auth_user_id):
     new_list = []
 
     for channel in initial_object['channels']:
-        for user in channel['all_members']:
-            if user['user_id'] == auth_user_id:
-                new_list.append({'channel_id' : channel['channel_id'],
-                'name': channel['name']})
+        for member in channel['all_members']:
+            for user in initial_object['users']:
+                if member['auth_user_id'] == user['auth_user_id']:
+                    print(member['auth_user_id'])
+                    new_list.append({'channel_id' : channel['channel_id'],
+                    'name': channel['name']})
+            
+                    
     return {'channels': new_list}
 
 def channels_listall_v1(auth_user_id):
@@ -35,34 +39,49 @@ def channels_listall_v1(auth_user_id):
         ],
     }
 
+# Creates a new channel with the given name that is either a public or private channel. 
 def channels_create_v1(auth_user_id, name, is_public):
-
+    '''    
+    return type: dict contains type 'channels_id' 
+    '''
+    
+    # error handling
     if len(name) not in range(1, 21):
         raise InputError('length of name is less than 1 or more than 20 characters')
     if name[0] == ' ':
         raise InputError('name cannot be blank')
     if is_public not in range(0,2):
         raise InputError('the channel has to be either public or private')
-    if not isinstance(auth_user_id,int):
+    if not isinstance(auth_user_id, int):
+        raise InputError('this is an invalid auth user id')
+    if not channels_create_check_valid_user(auth_user_id):
         raise InputError('this is an invalid auth user id')
 
-    store = data_store.get()
-    channels = store['channels']
+    channels = initial_object['channels']
     channel_id = len(channels) + 1
-    new = {
-        'channel_id': channel_id, 
-        'name': name, 
-        'is_public': is_public, 
-        'owner_members': [],
-        'all_members': []
-    }
-    
-    new['all_members'].append({'user_id': auth_user_id})
-    #print('number of channels', channel_id)
-    initial_object['channels'].append(new)
-    data_store.set(store)
+    owner = (channels_user_details(auth_user_id))
+    new = {'channel_id': channel_id, 'name': name, 'is_public': is_public, 'owner_members': [owner], 'all_members': [owner]}
+    channels.append(new)
     return {
-        'channel_id': new['channel_id']
+        'channel_id': channel_id,
     }
 
+# helper function to check if the auth_user_id given is registered
+def channels_create_check_valid_user(auth_user_id):
+    '''
+    return type: bool
+    '''
+    for user in initial_object['users']:
+        if user['auth_user_id'] == auth_user_id:
+            return True
+    return False
 
+# helper function to access to details of the given auth_user_id
+def channels_user_details(auth_user_id):
+    '''
+    return type: dict
+    '''
+    for user in initial_object['users']:
+        if user['auth_user_id'] == auth_user_id:
+            return user
+    return {}
