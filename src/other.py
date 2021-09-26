38055f1154ba6b5aa9edd991e8e9044cc2198384
Your Name <email@example.com>
2021-09-26 20:59:02 +1000

    completed

--- src/other.py
+++ src/other.py
@@ -3,4 +3,5 @@ from src.data_store import data_store, initial_object
 def clear_v1():
     store = data_store.get()
     initial_object['users'] = []
+    initial_object['channels'] = []
     data_store.set(store)
