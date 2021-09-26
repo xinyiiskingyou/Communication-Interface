0dd6a94913cfb050b05aea5004baf66b970f7b53
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-09-26 22:55:52 +1000

    changed the file name

---
+++ tests/channels_test.py
@@ -0,0 +1,65 @@
+import pytest
+from src.channels import channels_list_v2, channels_create_v1, channels_listall_v1
+from src.auth import auth_register_v1
+from src.other import clear_v1
+
+# test if a user is authorised but dosen't have channel
+# it should return empty
+def test_no_channels():
+	clear_v1()
+	no_channel = auth_register_v1('email1@gmail.com', 'Password1', 'anna','duong')
+	assert(channels_list_v2(no_channel['auth_user_id']) == {'channels':[]})
+
+# test if more and more authorised users can be appended in the list 
+def test_channels_list():
+	
+	clear_v1()
+	x_register = auth_register_v1('email@gmail.com', 'password', 'x', 'lin')
+	x_channel = channels_create_v1(x_register['auth_user_id'], 'x', True)
+	assert(channels_list_v2(x_register['auth_user_id']) ==
+		{
+		'channels':[
+		{
+			'channel_id': x_channel['channel_id'],
+			'name': 'x'
+        },
+		]
+		})
+
+	# test if there are 2 authorised users join the channels
+	# and including the private channels
+	sally_register = auth_register_v1('email2@gmail.com','comp1531', 'sally','zhou')
+	sally_channel = channels_create_v1(sally_register['auth_user_id'], 'sally', False)
+	assert(channels_list_v2(sally_register['auth_user_id']) == {
+		'channels': [
+			{
+				'channel_id': x_channel['channel_id'],
+				'name': 'x'
+			},
+			{
+				'channel_id': sally_channel['channel_id'],
+				'name': 'sally'
+			}
+		],
+	})
+
+	emma_register = auth_register_v1('email3@gmail.com', 'comp1521', 'emma','sun')
+	emma_channel = channels_create_v1(emma_register ['auth_user_id'],'emma', True)
+	assert(channels_list_v2(emma_register['auth_user_id']) == {
+		'channels': [
+			{
+				'channel_id': x_channel['channel_id'],
+				'name': 'x'
+			},
+
+			{
+				'channel_id': sally_channel['channel_id'],
+				'name': 'sally'
+			},
+
+			{
+				'channel_id': emma_channel['channel_id'],
+				'name': 'emma'
+			}
+		],
+	})
