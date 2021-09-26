d7b99dd602e2a56de62079f72f882e69900ac1b9
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-09-26 21:13:56 +1000

    updated

--- tests/channels_list_all_test.py
+++ tests/channels_list_all_test.py
@@ -2,6 +2,7 @@ import pytest
 from src.channels import channels_listall_v1, channels_create_v1
 from src.data_store import data_store, initial_object
 from src.auth import auth_register_v1
+from src.other import clear_v1
 def test_empty_channel(): 
     empty = auth_register_v1('email@gmail.com', 'password', 'hie', 'bye')
     assert(channels_listall_v1(empty['auth_user_id']) == {'channels':[]})
@@ -18,8 +19,11 @@ def test_channel_maker():
                 },]
         })
 
-def test_listall_channels(): 
+def test_listall_channels():
+    clear_v1()
     ash_register = auth_register_v1('ashley@gmail.com', 'ashpass', 'ashley', 'wong')
+    a_register = auth_register_v1('ashemail@gmail.com', 'password', 'anna', 'wong')
+    a_channel = channels_create_v1(a_register['auth_user_id'], 'anna', False)
     ash_channel = channels_create_v1(ash_register['auth_user_id'], 'ashley', False)
     ashv1_channel = channels_create_v1(ash_register['auth_user_id'], 'ash', True)
     assert (channels_listall_v1(ash_register['auth_user_id']) == 
@@ -39,3 +43,4 @@ def test_listall_channels():
                 }
             ],
         })
+    assert ((channels_listall_v1(ash_register['auth_user_id']))== (channels_list_v1(ash_register['auth_user_id'])))
