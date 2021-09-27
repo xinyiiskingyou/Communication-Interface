0b4a5b6df797b2fdc2b3eb71dd2a35bdb2472e1b
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-09-27 22:52:44 +1000

    added error tests

---
+++ tests/channeljoin_test.py
@@ -0,0 +1,18 @@
+from src.channels import channels_create_v1
+from src.channel import channel_join_v1
+from src.auth import auth_register_v1
+from src.data_store import data_store, initial_object
+from src.other import clear_v1
+
+
+def test_input_invalid_channel():
+    clear_v1()
+    with pytest.raise (InputError):
+        channel_join_v1(auth_user_id, 3456)
+
+def test_already_in(): 
+    clear_v1()
+    a_register = auth_register_v1{'email@gmail.com', 'password', 'lily','wong'}
+    a_channel = channels_create_v1(a_register['auth_user_id'], 'anna', True)
+    with pytest.raise (InputError): 
+        channel_join_v1(a_register['auth_user_id'], a_channel['channel_id'])
