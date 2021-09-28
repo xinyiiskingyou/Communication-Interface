393a6f47c6f9bcf49a556f55e05b5fafcbc8520f
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-09-28 13:07:56 +1000

    updated

--- src/channels.py
+++ src/channels.py
@@ -1,5 +1,5 @@
 from src.data_store import data_store, initial_object
-from src.error import InputError, AccessError
+from src.error import InputError
 
 def channels_list_v2(auth_user_id):
     '''
@@ -11,22 +11,21 @@ def channels_list_v2(auth_user_id):
     Return Value:
         returns channel_id and name if an authorised user has channel
     '''
-    # for all channels
-    # -> if the users are authorised
-    # -> append to an empty list
-    # return to the new list
-
     new_list = []
 
-    for channels in initial_object['channels']:
-        for member in channels['all_members']:
+    for channel in initial_object['channels']:
+        for member in channel['all_members']:
             for user in initial_object['users']:
+                # if the users are authorised (the auth_user_id can be found in the user list)
                 if member['auth_user_id'] == user['auth_user_id']:
-                    new_list.append({'channel_id' : channels['channel_id'],
-                    'name': channels['name']})       
+                    # append to an empty list
+                    new_list.append({'channel_id' : channel['channel_id'],
+                    'name': channel['name']})   
+    # return to the new list    
     return {'channels': new_list}
     
 def channels_listall_v1(auth_user_id):
+
     '''
     Provides a list of all channels, including private channels (and their associated 
     details)
@@ -38,6 +37,7 @@ def channels_listall_v1(auth_user_id):
     for channels in initial_object['channels']:
         listchannel.append({'channel_id' : channels['channel_id'], "name": channels['name']})
     return {'channels': listchannel}
+
 
 # Creates a new channel with the given name that is either a public or private channel. 
 def channels_create_v1(auth_user_id, name, is_public):
@@ -53,18 +53,25 @@ def channels_create_v1(auth_user_id, name, is_public):
     if is_public not in range(0,2):
         raise InputError('the channel has to be either public or private')
     if not isinstance(auth_user_id, int):
-        raise AccessError('this is an invalid auth user id')
+        raise InputError('this is an invalid auth user id')
     if not channels_create_check_valid_user(auth_user_id):
-        raise AccessError('this is an invalid auth user id')
+        raise InputError('this is an invalid auth user id')
 
     channels = initial_object['channels']
     # generate channel_id according the number of existing channels
     channel_id = len(channels) + 1
     owner = (channels_user_details(auth_user_id))
-    new = {'channel_id': channel_id, 'name': name, 'is_public': is_public, 'owner_members': [owner], 'all_members': [owner]}
+    new = {
+        'channel_id': channel_id, 
+        'name': name, 
+        'is_public': bool(is_public), 
+        'owner_members': [owner], 
+        'all_members': [owner]}
 
     channels.append(new)
-    return channel_id
+    return {
+        'channel_id': channel_id,
+    }
 
 # helper function to check if the auth_user_id given is registered
 def channels_create_check_valid_user(auth_user_id):
@@ -73,7 +80,7 @@ def channels_create_check_valid_user(auth_user_id):
     '''
     for user in initial_object['users']:
         if user['auth_user_id'] == auth_user_id:
-            return True
+            return user['auth_user_id']
     return False
 
 # helper function to access to details of the given auth_user_id
