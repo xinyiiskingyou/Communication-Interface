8055e502e6e566ba9cd196cf901724377533b6d9
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-09-28 16:52:43 +1000

    implementation done

--- src/channel.py
+++ src/channel.py
@@ -59,9 +59,47 @@ def channel_join_v1(auth_user_id, channel_id):
     ''' 
     if channels_create_check_valid_user(auth_user_id) == False:
         raise AccessError ('Auth_user_id is not a valid id')
-    if 
-
-
-
+    if check_valid_channel_id(channel_id) == False: 
+        raise InputError('Channel id is not valid')
+    if check_valid_member_in_channel(channel_id, auth_user_id) == True
+        raise InputError ('Already a member of this channel')
+    elif check_valid_member_in_channel (channel_id, auth_user_id) == False: 
+        if check_channel_private(channel_id) == True: 
+            raise AccessError ('Not authorised to join channel')
+    store = data_store.get()
+    for channels in initial_object['channels']: 
+        if channels['channel_id'] == channel_id:
+            channels['all_members'].append(auth_user_id)
     return {
     }
+
+def check_valid_channel_id(channel_id):
+    store = data_store.get()
+    for channel in initial_object['channels']:
+        if channel['channel_id'] == channel_id:
+            return channel
+    return False
+
+
+def check_valid_member_in_channel(channel_id, auth_user_id):
+
+    # for all channels
+    # if the user has channel_id
+    # if the users are authorised
+    # return True
+    for channel in initial_object['channels']:
+        if channel['channel_id'] == channel_id:
+            for member in channel['all_members']:
+                if member['auth_user_id'] == auth_user_id:
+                    return True
+    
+    return False
+
+def check_channel_private(channel_id): 
+    store = data_store.get()
+    for channel in initial_object['channels']:
+        if channel['channel_id'] = channel_id: 
+            if channel['is_public'] == False: 
+                return True
+            else: 
+                return False
