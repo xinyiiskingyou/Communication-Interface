90faa29d1a5a2cd435e50434a56846774549d6bc
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-09-28 16:04:10 +1000

    dunno what happened but it kinda passed the tests

--- tests/channel_invite_test.py
+++ tests/channel_invite_test.py
@@ -2,57 +2,52 @@ import pytest
 from src.channel import channel_invite_v1
 from src.error import InputError, AccessError
 from src.other import clear_v1
-from src.channels import channels_create_v1, channels_list_v2
+from src.channels import channels_create_v1, channels_list_v2, channels_listall_v1
 from src.auth import auth_register_v1
 
+# create a public channel
 x_register = auth_register_v1('email@gmail.com', 'password', 'x', 'lin')
-x_channel = channels_create_v1(x_register['auth_user_id'], 'x', True)
-
-sally_register = auth_register_v1('email1@gmail.com','comp1531', 'sally','zhou')
-sally_channel = channels_create_v1(sally_register['auth_user_id'], 'sally', False)
-
+x_channel = channels_create_v1(x_register, 'x', True)
+# create a private channel
+sally_register = auth_register_v1('email1@gmail.com', 'comp1531', 'sally', 'zhou')
+sally_channel = channels_create_v1(sally_register, 'sally', False)
+# create an auth_user but currently did not join the channel
 y_register = auth_register_v1('email2@gmail.com', 'password', 'y', 'lin')
 
-'''
 # input error
 def test_channel_invite_input_error():
 
     with pytest.raises(InputError):
         # channel_id does not refer to a valid channel
-        channel_invite_v1(x_register['auth_user_id'], 1234, y_register['auth_user_id'])
-        channel_invite_v1(sally_register['auth_user_id'], 456, y_register['auth_user_id'])
+        channel_invite_v1(x_register, 1234, y_register)
+        channel_invite_v1(sally_register, 456, y_register)
         
         # u_id does not refer to a valid user
-        channel_invite_v1(x_register['auth_user_id'], x_channel['channel_id'], 'hello')
-        channel_invite_v1(sally_register['auth_user_id'], sally_channel['channel_id'], 'baka')
+        channel_invite_v1(x_register, x_channel, '0')
+        channel_invite_v1(sally_register, sally_channel, '-1')
 
-
-# Access error when the authorised user is not a member of the channel
+# Access error when channel_id is valid and the authorised user is not a member of the channel
 def test_channel_invite_access_error():
-
-    clear_v1()
     with pytest.raises(AccessError):
-        channel_invite_v1(y_register['auth_user_id'], x_channel['channel_id'], y_register['auth_user_id'])
+        channel_invite_v1(x_register, sally_channel, y_register)
 
-'''
+y_channel = channels_create_v1(y_register, 'y', False)
 def test_valid_invite_public_member():
 
 	# test if public channel member can invite new user
-	channel_invite_v1(x_register['auth_user_id'], x_channel['channel_id'], y_register['auth_user_id'])
-	y_channel = channels_create_v1(y_register['auth_user_id'], 'y', False)
-
-	assert(channels_list_v2(x_register['auth_user_id']) == {
+	channel_invite_v1(x_register, x_channel, y_register)
+	assert(channels_listall_v1(x_register) == {
 		'channels': [
 			{
-				'channel_id': x_channel['channel_id'],
+				'channel_id': x_channel,
 				'name': 'x'
 			},
 			{
-				'channel_id': sally_channel['channel_id'],
+				'channel_id': sally_channel,
 				'name': 'sally'
 			},
 			{
-				'channel_id': y_channel['channel_id'],
+				'channel_id': y_channel,
 				'name': 'y'
 			}
 		],
@@ -60,25 +55,25 @@ def test_valid_invite_public_member():
 
 	# test if private channel member can invite new user
 	z_register = auth_register_v1('email3@gmail.com', 'password', 'z', 'lin')
-	channel_invite_v1(sally_register['auth_user_id'], sally_channel['channel_id'], z_register['auth_user_id']) 
-	z_channel = channels_create_v1(z_register['auth_user_id'], 'z', True)
+	channel_invite_v1(sally_register, sally_channel, z_register) 
+	z_channel = channels_create_v1(z_register, 'z', True)
 
-	assert(channels_list_v2(sally_register['auth_user_id']) == {
+	assert(channels_listall_v1(sally_register) == {
 		'channels': [
 			{
-				'channel_id': x_channel['channel_id'],
+				'channel_id': x_channel,
 				'name': 'x'
 			},
 			{
-				'channel_id': sally_channel['channel_id'],
+				'channel_id': sally_channel,
 				'name': 'sally'
 			},
 			{
-				'channel_id': y_channel['channel_id'],
+				'channel_id': y_channel,
 				'name': 'y'
 			},
 			{
-				'channel_id': z_channel['channel_id'],
+				'channel_id': z_channel,
 				'name': 'z'
 			}
 			
