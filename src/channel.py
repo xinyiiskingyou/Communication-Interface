28dd693b96d8c73dfe932e6f27d39a5bbfbc1b6b
Sree Varada Raju <sreevraju@gmail.com>
2021-09-28 15:38:37 +1000

    Passed tests and implemented functions. Unsure of how to return the information

--- src/channel.py
+++ src/channel.py
@@ -1,28 +1,25 @@
+from src.data_store import data_store, initial_object
+from src.error import InputError, AccessError
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     return {
     }
 
 def channel_details_v1(auth_user_id, channel_id):
+    # Inavlid channel_id
+    if check_valid_channel_id(channel_id) == False:
+        raise InputError("The channel_id does not refer to a valid channel")
+
+    # Authorised user not a member of channel
+    if check_valid_member_in_channel(channel_id, auth_user_id) == False:
+        raise AccessError("Authorised user is not a member of channel with channel_id")
+
+    channel_info = check_valid_channel_id(channel_id)
     return {
-        'name': 'Hayden',
-        'owner_members': [
-            {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
-            }
-        ],
-        'all_members': [
-            {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
-            }
-        ],
+        'name': channel_info['name'],
+        'is_public': channel_info['is_public'],
+        'owner_members': channel_info['owner_members'],
+        'all_members': channel_info['all_members'],
     }
 
 def channel_messages_v1(auth_user_id, channel_id, start):
@@ -42,3 +39,22 @@ def channel_messages_v1(auth_user_id, channel_id, start):
 def channel_join_v1(auth_user_id, channel_id):
     return {
     }
+
+# Helper function for channel_details
+# Checks if a valid channel_id is being passed in or not
+def check_valid_channel_id(channel_id):
+    store = data_store.get()
+    for channel in initial_object['channels']:
+        if channel['channel_id'] == channel_id:
+            return channel
+    return False
+
+# Check if user with valid channel_id is not a member of the channel
+def check_valid_member_in_channel(channel_id, auth_user_id):
+    for channel in initial_object['channels']:
+        if channel['channel_id'] == channel_id:
+            for member in channel['all_members']:
+                if member['auth_user_id'] == auth_user_id:
+                    return True
+    return False
+
--- tests/channel_test.py
+++ tests/channel_test.py
@@ -2,6 +2,7 @@ import pytest
 from src.other import clear_v1
 from src.auth import auth_register_v1
 from src.channels import channels_list_v2, channels_create_v1
+from src.channel import channel_details_v1
 from src.error import InputError, AccessError
 
 # Public: Invalid channel_id 
@@ -23,9 +24,9 @@ def test_c_details_invalid_id():
     channel_id = channels_create_v1(auth_user_id2, 'anna', False)
     
     with pytest.raises(InputError):
-        channel_details_v1(auth_user_id1, 0)
+        channel_details_v1(auth_user_id2, 0)
         channel_details_v1(auth_user_id2, -1)
-        channel_details_v1(auth_user_id1, -116)
+        channel_details_v1(auth_user_id2, -116)
 
 # Public: Authorised user is not a memner of the channel
 def test_c_details_not_member():
@@ -43,4 +44,4 @@ def test_c_details_not_member():
     auth_user_id3 = auth_register_v1('cat@gmail.com', 'password', 'name_first', 'name_last')
     channel_id = channels_create_v1(auth_user_id2, 'anna', False)
     with pytest.raises(AccessError):
-         channel_details_v1(auth_user_id1, 1)
+         channel_details_v1(auth_user_id3, 1)
