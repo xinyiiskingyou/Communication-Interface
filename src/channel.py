89ad8b430f327d938bbdf41bbaf19184143d1372
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-09-28 17:47:34 +1000

    updated

--- src/channel.py
+++ src/channel.py
@@ -8,27 +8,22 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
     }
 
 def channel_details_v1(auth_user_id, channel_id):
+    # Inavlid channel_id
+    if check_valid_channel_id(channel_id) == False:
+        raise InputError("The channel_id does not refer to a valid channel")
+
+    # Authorised user not a member of channel
+    if check_valid_member_in_channel(channel_id, auth_user_id) == False:
+        raise AccessError("Authorised user is not a member of channel with channel_id")
+
+    channel_info = check_valid_channel_id(channel_id)
     return {
-        'name': 'Hayden',
-        'owner_members': [
-            {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
-            }
-        ],
-        'all_members': [
-            {
-                'u_id': 1,
-                'email': 'example@gmail.com',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'haydenjacobs',
-            }
-        ],
+        'name': channel_info['name'],
+        'is_public': channel_info['is_public'],
+        'owner_members': channel_info['owner_members'],
+        'all_members': channel_info['all_members'],
     }
+
 
 def channel_messages_v1(auth_user_id, channel_id, start):
     return {
@@ -86,9 +81,9 @@ def check_valid_member_in_channel(channel_id, auth_user_id):
     # if the user has channel_id
     # if the users are authorised
     # return True
-    for channel in initial_object['channels']:
-        if channel['channel_id'] == channel_id:
-            for member in channel['all_members']:
+    for channels in initial_object['channels']:
+        if channels['channel_id'] == channel_id:
+            for member in channels['all_members']:
                 if member['auth_user_id'] == auth_user_id:
                     return True
     
