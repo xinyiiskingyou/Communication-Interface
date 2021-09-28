8ec39bd6326e0c18cb1ad88803907c75ccba033e
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-09-28 23:16:49 +1000

    updated

--- tests/channel_test.py
+++ tests/channel_test.py
@@ -1,5 +1,5 @@
 import pytest
-from src.channel import channel_invite_v1, channel_details_v1
+from src.channel import channel_invite_v1, channel_details_v1, channel_join_v1
 from src.error import InputError, AccessError
 from src.channels import channels_create_v1, channels_listall_v1
 from src.auth import auth_register_v1
@@ -113,3 +113,41 @@ def test_valid_invite_member_private():
 			}
 		],
 	})
+
+
+def test_input_invalid_channel():
+    clear_v1()
+    a_register = auth_register_v1('email@gmail.com', 'password', 'lily','wong')
+    with pytest.raises(InputError):
+        channel_join_v1(a_register, 3456)
+
+def test_already_in(): 
+    clear_v1()
+    a_register = auth_register_v1('email@gmail.com', 'password', 'lily','wong')
+    a_channel = channels_create_v1(a_register, 'anna', True)
+    with pytest.raises(InputError): 
+        channel_join_v1(a_register, a_channel)
+    
+def test_AccessError (): 
+    clear_v1()
+    a_register = auth_register_v1('email@gmail.com', 'password', 'lily','wong')
+    a_channel = channels_create_v1(a_register, 'anna', False)
+    j_register = auth_register_v1('ashemail@gmail.com', 'password', 'jilly','wong')
+    with pytest.raises(AccessError): 
+            channel_join_v1(j_register, a_channel)
+
+
+def test_authuser_AccessError (): 
+    clear_v1()
+    a_register = auth_register_v1('email@gmail.com', 'password', 'lily','wong')
+    a_channel = channels_create_v1(a_register, 'anna', True)
+    with pytest.raises(AccessError):
+        channel_join_v1(123, a_channel)
+
+def test_join (): 
+    clear_v1()
+    a_register = auth_register_v1('email@gmail.com', 'password', 'lily','wong')
+    a_channel = channels_create_v1(a_register, 'anna', True)
+    j_register = auth_register_v1('ashemail@gmail.com', 'password', 'jilly','wong')
+    channel_join_v1(j_register, a_channel)
+    assert channel_details_v1(j_register, a_channel) == channel_details_v1(a_register, a_channel)
