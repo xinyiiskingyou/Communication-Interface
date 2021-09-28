23f0087e2db849c38214880626f556d64788f0d7
Sree Varada Raju <sreevraju@gmail.com>
2021-09-28 18:59:09 +1000

    changed return

--- src/channel.py
+++ src/channel.py
@@ -14,12 +14,11 @@ def channel_details_v1(auth_user_id, channel_id):
     if check_valid_member_in_channel(channel_id, auth_user_id) == False:
         raise AccessError("Authorised user is not a member of channel with channel_id")
 
-    channel_info = check_valid_channel_id(channel_id)
     return {
-        'name': channel_info['name'],
-        'is_public': channel_info['is_public'],
-        'owner_members': channel_info['owner_members'],
-        'all_members': channel_info['all_members'],
+        'name': channels['name'],
+        'is_public': channels['is_public'],
+        'owner_members': channels['owner_members'],
+        'all_members': channels['all_members'],
     }
 
 def channel_messages_v1(auth_user_id, channel_id, start):
@@ -46,7 +45,7 @@ def check_valid_channel_id(channel_id):
     store = data_store.get()
     for channel in initial_object['channels']:
         if channel['channel_id'] == channel_id:
-            return channel
+            return True
     return False
 
 # Check if user with valid channel_id is not a member of the channel
