64c0ac3952dbfc2ee155443dccf7ccc861861bf0
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-09-29 23:01:17 +1000

    updated the way check u_id

--- src/channel.py
+++ src/channel.py
@@ -2,8 +2,6 @@ from src.data_store import data_store, initial_object
 from src.error import InputError, AccessError
 from src.channels import channels_create_check_valid_user, channels_user_details
 
-
-
 def channel_invite_v1(auth_user_id, channel_id, u_id):
 
     '''
@@ -12,13 +10,12 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
     In both public and private channels, all members are able to invite users.
 
     '''
-
-    # Input error
+    # Input error when channel_id does not refer to a valid channel
     if check_channel_id(channel_id) == False:
         raise InputError("Channel_id does not refer to a valid channel")
     
-    # how do you check valid u_id
-    if check_valid_u_id(u_id) == False:
+    # Input error when u_id does not refer to a valid user
+    if int(u_id) > len(initial_object['users']) or int(u_id) < 0:
         raise InputError("u_id does not refer to a valid user")
     
     # Access error channel_id is valid and the authorised user is not a member of the channel
@@ -34,8 +31,8 @@ def channel_invite_v1(auth_user_id, channel_id, u_id):
             
     return {}
 
-
 def channel_details_v1(auth_user_id, channel_id):
+
    # Inavlid channel_id
     if check_channel_id(channel_id) == False:
         raise InputError("The channel_id does not refer to a valid channel")
@@ -77,30 +74,28 @@ def channel_join_v1(auth_user_id, channel_id):
     - when a channel is private and authorised user is not a member - also not global owner 
 
     to do: 
-    take in an authuserid and channel id and append them to the member list in channels dictionary 
+    take in an auth_user_id and channel id and append them to the member list in channels dictionary 
     ''' 
     if channels_create_check_valid_user(auth_user_id) == False:
         raise AccessError ('Auth_user_id is not a valid id')
+
     if check_channel_id(channel_id) == False: 
         raise InputError('Channel id is not valid')
+
     if check_valid_member_in_channel(channel_id, auth_user_id) == True:
         raise InputError ('Already a member of this channel')
+
     elif check_valid_member_in_channel (channel_id, auth_user_id) == False: 
         if check_channel_private(channel_id) == True: 
             raise AccessError ('Not authorised to join channel')
+            
     new_user = channels_user_details(auth_user_id)
     for channels in initial_object['channels']: 
         if channels['channel_id'] == channel_id:
             channels['all_members'].append(new_user)
+
     return {
     }
-
-# A helper function to check for valid u_id
-def check_valid_u_id(u_id):
-    # negative u_id
-    if int(u_id) < 0:
-        return False
-    return True
 
 # Checks if a valid channel_id is being passed in or not
 def check_channel_id(channel_id):
@@ -108,7 +103,6 @@ def check_channel_id(channel_id):
         if int(channel['channel_id']) == int(channel_id):
             return channel
     return False
-
 
 def check_valid_member_in_channel(channel_id, auth_user_id):
     for channel in initial_object['channels']:
