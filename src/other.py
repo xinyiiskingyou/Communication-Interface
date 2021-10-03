80ca98093caff345ade8ffb95cdf8cf8b37b1c5d
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-10-03 13:46:53 +1100

    ...

--- src/other.py
+++ src/other.py
@@ -1,6 +1,9 @@
 from src.data_store import data_store, initial_object
 
 def clear_v1():
+    '''
+    Clear the data that has been stored
+    '''
     store = data_store.get()
     initial_object['users'] = []
     initial_object['channels'] = []
