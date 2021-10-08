dd5cc002686c5c922267f379fdc97d66091eeea1
Sree Varada Raju <sreevraju@gmail.com>
2021-10-08 21:07:27 +1100

    changed params= to data= in http_auth_test.py file

--- tests/http_auth_test.py
+++ tests/http_auth_test.py
@@ -11,26 +11,26 @@ BASE_URL = 'http://127.0.0.1:8080'
 
 def test_reg_invalid_email():
     clear_v1()
-    resp1 = requests.get(config.url + 'register', params={'email': 'abc'})
-    resp2 = requests.get(config.url + 'register', params={'email': 'abc@gmail'})
+    resp1 = requests.post(config.url + 'register', data={'email': 'abc'})
+    resp2 = requests.post(config.url + 'register', data={'email': 'abc@gmail'})
     assert(resp1.status_code == 400)
     assert(resp2.status_code == 400)
 
 def test_reg_duplicate_email():
     clear_v1()
-    resp1 = requests.get(config.url + 'register', params={'email': 'abc@gmail.com'})
+    resp1 = requests.post(config.url + 'register', data={'email': 'abc@gmail.com'})
     assert(resp1.status_code == 400)
-    resp2 = requests.get(config.url + 'register', params={'email': 'abc@gmail.com'})
+    resp2 = requests.post(config.url + 'register', data={'email': 'abc@gmail.com'})
 
 def test_reg_invalid_password():
     clear_v1()
-    resp1 = requests.get(config.url + 'register', params={'password': '12345'})
+    resp1 = requests.post(config.url + 'register', data={'password': '12345'})
     assert(resp1.status_code == 400)
 
 def test_reg_invalid_name():
     clear_v1()
-    resp1 = requests.get(config.url + 'register', params={'name_first': 'a' * 50})
-    resp2 = requests.get(config.url + 'register', params={'last_first': 'a' * 50})
+    resp1 = requests.post(config.url + 'register', data={'name_first': 'a' * 50})
+    resp2 = requests.post(config.url + 'register', data={'last_first': 'a' * 50})
     assert(resp1.status_code == 400)
     assert(resp2.status_code == 400)
 
