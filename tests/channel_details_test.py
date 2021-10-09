c3b55759141fa70dcf7a86c2a8f014b5396dff3a
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-10-10 00:24:00 +1100

    updated to token

--- tests/channel_details_test.py
+++ tests/channel_details_test.py
@@ -3,98 +3,49 @@ Test channel_details function
 '''
 
 import pytest
-from src.channel import channel_invite_v1, channel_details_v1
+from src.channel import channel_invite_v2, channel_details_v2
+from src.channels import channels_create_v2
 from src.error import InputError, AccessError
-from src.channels import channels_create_v1
-from src.auth import auth_register_v1
+from src.auth import auth_register_v2
 from src.other import clear_v1
-
-clear_v1()
-
-id1 = auth_register_v1('abc@gmail.com', 'password', 'afirst', 'alast')
-id2 = auth_register_v1('email@gmail.com', 'password', 'bfirst', 'blast')
-id3 = auth_register_v1('elephant@gmail.com', 'password', 'cfirst', 'clast')
-id4 = auth_register_v1('cat@gmail.com', 'password', 'dfirst', 'dlast')
-channel_id2 = channels_create_v1(id2['auth_user_id'], 'anna', True)
-channel_id4 = channels_create_v1(id4['auth_user_id'], 'shelly', False)
 
 ##########################################
 ######### channel_details tests ##########
 ##########################################
 
-# Access Error
-def test_details_invalid_auth_user_id():
-    '''
-    Invalid auth_user_id
-    '''
-    # Public
-    with pytest.raises(AccessError):
-        channel_details_v1(-1, channel_id2['channel_id'])
-    with pytest.raises(AccessError):
-        channel_details_v1(0, channel_id2['channel_id'])
-    with pytest.raises(AccessError):
-        channel_details_v1(256, channel_id2['channel_id'])
-    with pytest.raises(AccessError):
-        channel_details_v1('not_an_id', channel_id2['channel_id'])
-    with pytest.raises(AccessError):
-        channel_details_v1('', channel_id2['channel_id'])
-
-    # Private
-    with pytest.raises(AccessError):
-        channel_details_v1(-1, channel_id4['channel_id'])
-    with pytest.raises(AccessError):
-        channel_details_v1(0, channel_id4['channel_id'])
-    with pytest.raises(AccessError):
-        channel_details_v1(256, channel_id4['channel_id'])
-    with pytest.raises(AccessError):
-        channel_details_v1('not_an_id', channel_id4['channel_id'])
-    with pytest.raises(AccessError):
-        channel_details_v1('', channel_id4['channel_id'])
-
-# Input Error
+# Input Error when channel_id does not refer to a valid channel
 def test_details_invalid_channel_id():
-    '''
-    Invalid channel_id
-    '''
-    with pytest.raises(InputError):
-        channel_details_v1(id1['auth_user_id'], -1)
-    with pytest.raises(InputError):
-        channel_details_v1(id1['auth_user_id'], 0)
-    with pytest.raises(InputError):
-        channel_details_v1(id1['auth_user_id'], 256)
-    with pytest.raises(InputError):
-        channel_details_v1(id1['auth_user_id'], 'not_an_id')
-    with pytest.raises(InputError):
-        channel_details_v1(id1['auth_user_id'], '')
 
-# Raises Access Error as both Input and Access error are raised
-def test_details_invalid_auth_user_id_and_channel_id():
-    '''
-    Invalid auth_user_id and invalid channel_id
-    '''
-    with pytest.raises(AccessError):
-        channel_details_v1('', -16)
-    with pytest.raises(AccessError):
-        channel_details_v1('not_an_id', 0)
-    with pytest.raises(AccessError):
-        channel_details_v1(256, 256)
-    with pytest.raises(AccessError):
-        channel_details_v1(0, 'not_an_id')
-    with pytest.raises(AccessError):
-        channel_details_v1(-16, '')
+    clear_v1()
+    id1 = auth_register_v2('abc@gmail.com', 'password', 'afirst', 'alast')
+    with pytest.raises(InputError):
+        channel_details_v2(id1['token'], -1)
+    with pytest.raises(InputError):
+        channel_details_v2(id1['token'], 0)
+    with pytest.raises(InputError):
+        channel_details_v2(id1['token'], 256)
+    with pytest.raises(InputError):
+        channel_details_v2(id1['token'], 'not_an_id')
+    with pytest.raises(InputError):
+        channel_details_v2(id1['token'], '')
 
-# Access Error
+# Access Error when authorised user is not a member of the channel
 def test_details_not_member():
-    '''
-    Authorised user is not a member of the channel
-    '''
+
+    clear_v1()
+    id1 = auth_register_v2('abc@gmail.com', 'password', 'afirst', 'alast')
+    id2 = auth_register_v2('email@gmail.com', 'password', 'bfirst', 'blast')
+    id3 = auth_register_v2('cat@gmail.com', 'password1', 'cfirst', 'clast')
+    channel_id = channels_create_v2(id2['token'], 'anna', True)
+    channel_id1 = channels_create_v2(id3['token'], 'a', False)
+
     # Public
     with pytest.raises(AccessError):
-        channel_details_v1(id3['auth_user_id'], channel_id2['channel_id'])
+        channel_details_v2(id1['token'], channel_id['channel_id'])
 
     # Private
     with pytest.raises(AccessError):
-        channel_details_v1(id3['auth_user_id'], channel_id4['channel_id'])
+        channel_details_v2(id1['token'], channel_id1['channel_id'])
 
 ##### Implementation #####
 def test_details_valid_channel():
@@ -102,11 +53,11 @@ def test_details_valid_channel():
     Test correct details for valid channel
     '''
     clear_v1()
-    a_id = auth_register_v1('abc@gmail.com', 'password', 'afirst', 'alast')
-    new_id = auth_register_v1('email@gmail.com', 'password', 'afirst', 'alast')
-    channel_id = channels_create_v1(a_id['auth_user_id'], 'anna', True)
+    a_id = auth_register_v2('abc@gmail.com', 'password', 'afirst', 'alast')
+    new_id = auth_register_v2('email@gmail.com', 'password', 'afirst', 'alast')
+    channel_id = channels_create_v2(a_id['token'], 'anna', True)
 
-    assert(channel_details_v1(a_id['auth_user_id'], channel_id['channel_id']) ==
+    assert(channel_details_v2(a_id['token'], channel_id['channel_id']) ==
         {
         'name': 'anna',
         'is_public': True,
@@ -131,11 +82,9 @@ def test_details_valid_channel():
         ]
     })
 
-    channel_invite_v1(a_id['auth_user_id'], channel_id['channel_id'], new_id['auth_user_id'])
-    len1 = len(channel_details_v1(a_id['auth_user_id'], channel_id['channel_id'])['owner_members'])
+    channel_invite_v2(a_id['token'], channel_id['channel_id'], new_id['auth_user_id'])
+    len1 = len(channel_details_v2(a_id['token'], channel_id['channel_id'])['owner_members'])
     assert len1 == 1
 
-    len2 = len(channel_details_v1(a_id['auth_user_id'], channel_id['channel_id'])['all_members'])
+    len2 = len(channel_details_v2(a_id['token'], channel_id['channel_id'])['all_members'])
     assert len2 == 2
-
-clear_v1()
