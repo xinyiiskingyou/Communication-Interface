3ddaa4f091a8d88fe94f4d49b1ef0d622bf768e5
Your Name <email@example.com>
2021-10-10 13:23:29 +1100

    copy and pasted from master

--- tests/channels_test.py
+++ tests/channels_test.py
@@ -1,10 +1,12 @@
 import pytest
-from src.channels import channels_list_v1, channels_create_v1, channels_listall_v1
+from src.channels import channels_listall_v2, channels_create_v2, channels_listall_v2
 from src.channel import channel_invite_v1, channel_join_v1
 from src.auth import auth_register_v1
 from src.other import clear_v1
 from src.error import InputError, AccessError
-
+from src import config
+import requests
+import json
 ##########################################
 ######### channels_create tests ##########
 ##########################################
