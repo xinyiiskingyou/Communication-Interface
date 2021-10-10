a403fcf16d2556170af62c2fcdf1088b1d505f88
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-10-10 14:39:02 +1100

    updated

--- src/server.py
+++ src/server.py
@@ -7,7 +7,7 @@ from src.error import InputError
 from src import config
 
 from src.auth import auth_register_v2
-from src.channels import channels_list_v2, channels_create_v2, channels_listall_v2
+from src.channels import channels_list_v2, channels_create_v2
 from src.other import clear_v1
 
 def quit_gracefully(*args):
@@ -43,12 +43,11 @@ def echo():
         'data': data
     })
 
-
 # To clear the data
 @APP.route("/clear/v1", methods=['DELETE'])
 def clear():
-    return dumps({ 
-    })
+    resp = clear_v1()
+    return dumps(resp)
 
 #still uses auth_user_id have to fix auth implementation to generate a token
 @APP.route("/auth/register/v2", methods=['POST'])
@@ -60,18 +59,20 @@ def register():
         'auth_user_id': resp['auth_user_id']
     })
 
+############ CHANNELS #################
 @APP.route("/channels/create/v2", methods=['POST'])
 def channel_create():
     json = request.get_json()
-    resp = channels_create_v2(json['token'], json['name'], json['is_public'])
+    resp1 = channels_create_v2(json['token'], json['name'], bool(json['is_public']))
     return dumps({
-        'channel_id': resp['channel_id']
+        'channel_id': resp1['channel_id']
     })
 
 @APP.route("/channels/list/v2", methods=['GET'])
 def channels_list(): 
-    token = request.args.get('token')
-    return dumps(channels_list_v2(token))
+    json = request.get_json()
+    resp = channels_list_v2(json['token'])
+    return dumps(resp)
 
 #### NO NEED TO MODIFY BELOW THIS POINT
 
