da75d81b86b8e9a48c9a4df6b7497914dd602d4b
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-10-12 22:08:10 +1100

    added a http test

--- tests/http_channel_leave_test.py
+++ tests/http_channel_leave_test.py
@@ -4,7 +4,9 @@ from src.channels import channels_create_v2
 from src.error import AccessError, InputError
 from src.other import clear_v1
 from src.auth import auth_register_v2
-
+import requests
+import json 
+from src import config
 
 #invalid channel_id 
 def test_leave_invalid_channel_id():
@@ -67,3 +69,48 @@ def test_leave_valid_public_channel():
 
     details3 = channel_details_v2(id2['token'], channel_id2['channel_id'])
     assert len(details3['all_members']) == 1
+
+
+def test_http_valid(): 
+    requests.delete(config.url + "clear/v1")
+
+    user1 = requests.post(config.url + "auth/register/v2", 
+        json = {
+            'email': 'abc@gmail.com',
+            'password': 'password',
+            'name_first': 'anna',
+            'name_last': 'park'
+        })
+    user2 = requests.post(config.url + "auth/register/v2", 
+        json = {
+            'email': 'email@gmail.com',
+            'password': 'password',
+            'name_first': 'john',
+            'name_last': 'doe'
+        })
+    token1 = json.loads(user1.text)['token']
+    token2 = json.loads(user2.text)['token']
+
+    channel1 = requests.post(config.url + "channels/create/v2", 
+        json = {
+        'token': token1,
+        'name': 'channel1',
+        'is_public': True
+    })
+    channel_id1 = json.loads(channel1.text)['channel_id']
+    assert channel_id1 == json.loads(channel1.text)['channel_id']
+
+    respo1 = requests.post(config.url + "channel/join/v2",
+        json = { 
+        'token': token2, 
+        'channel_id': json.loads(channel1.text)['channel_id'],
+        })   
+    assert respo1.status_code == 200
+
+    respo = requests.post(config.url + "channel/leave/v1",
+        json = { 
+        'token': token1, 
+        'channel_id': json.loads(channel1.text)['channel_id'],
+        })  
+    
+    assert respo.status_code == 200
