c1812755877098060b0c83d1d9f8b3b6f3db512d
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-10-15 00:09:50 +1100

    added error tests for dm messages

--- tests/http_dm_tests.py
+++ tests/http_dm_tests.py
@@ -14,13 +14,31 @@ from src.dm import dm_details_v1
 def test_dm_details_error_token(): 
     clear_v1()
     with pytest.raises(InputError):
-        dm_details_v1('asjasd','')
+        dm_messages_v1('asjasd','')
 
 def test_dm_details_error_dm_id(): 
     clear_v1
     id1 = auth_register_v2('abc1@gmail.com', 'password', 'afirst', 'alast')
     with pytest.raises(InputError): 
-        dm_details_v1(id1['token'], 'jasjdlak')
+        dm_messages_v1(id1['token'], 'jasjdlak')
 
 ##add valid tests 
-    
+
+
+
+
+##########################################
+#########   Dm messages tests   ##########
+##########################################
+
+def test_dm_messages_error_token(): 
+    clear_v1()
+    with pytest.raises(InputError):
+        dm_messages_v1('asjasd','', 5)
+
+def test_dm_messages_error_dm_id(): 
+    clear_v1
+    id1 = auth_register_v2('abc1@gmail.com', 'password', 'afirst', 'alast')
+    with pytest.raises(InputError): 
+        dm_messages_v1(id1['token'], 'jasjdlak', 5)
+
