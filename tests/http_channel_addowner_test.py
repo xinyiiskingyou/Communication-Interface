b0cfaa73d6c6c629f2e2cabc558a0406a5f3a29b
Your Name <email@example.com>
2021-10-15 15:37:13 +1100

    added more tests when conditions of input error and access error both triggered

--- tests/http_channel_addowner_test.py
+++ tests/http_channel_addowner_test.py
@@ -283,6 +283,197 @@ def test_no_perm_not_owner():
         })
     assert resp1.status_code == 403
 
+def test_no_perm_not_owner_invalid_u_id():
+    requests.delete(config.url + "clear/v1")
+    id1 = requests.post(config.url + "auth/register/v2", 
+        json = {
+            'email': 'abc@gmail.com',
+            'password': 'password',
+            'name_first': 'afirst',
+            'name_last': 'alast'
+        })
+
+    token1 = json.loads(id1.text)['token']
+
+    ch1 = requests.post(config.url + "channels/create/v2", 
+        json = {
+            'token': token1,
+            'name': '1531_CAMEl',
+            'is_public': True
+        })
+    channel_id = json.loads(ch1.text)['channel_id']
+
+    id2 = requests.post(config.url + "auth/register/v2", 
+        json ={
+            'email': 'elephant@gmail.com',
+            'password': 'password',
+            'name_first': 'kelly',
+            'name_last': 'huang'
+            })
+    token2 = json.loads(id2.text)['token']
+    u_id1 = json.loads(id2.text)['auth_user_id']
+
+    id3 = requests.post(config.url + "auth/register/v2", 
+        json ={
+            'email': 'apple@gmail.com',
+            'password': 'password',
+            'name_first': 'hello',
+            'name_last': 'world'
+            })
+    u_id2 = json.loads(id3.text)['auth_user_id']
+
+    requests.post(config.url + 'channel/invite/v2', 
+        json ={
+            'token': token1,
+            'channel_id': channel_id,
+            'u_id': u_id1
+        })
+    
+    resp1 = requests.post(config.url + "channel/addowner/v1", 
+        json = {
+            'token': token2,
+            'channel_id': channel_id,
+            'u_id': -1
+        })
+
+    resp2 = requests.post(config.url + "channel/addowner/v1", 
+        json = {
+            'token': token2,
+            'channel_id': channel_id,
+            'u_id': 123
+        })
+    assert resp1.status_code == 403
+    assert resp2.status_code == 403
+
+def test_no_perm_not_owner_u_id_not_member():
+    requests.delete(config.url + "clear/v1")
+    id1 = requests.post(config.url + "auth/register/v2", 
+        json = {
+            'email': 'abc@gmail.com',
+            'password': 'password',
+            'name_first': 'afirst',
+            'name_last': 'alast'
+        })
+
+    token1 = json.loads(id1.text)['token']
+
+    ch1 = requests.post(config.url + "channels/create/v2", 
+        json = {
+            'token': token1,
+            'name': '1531_CAMEl',
+            'is_public': True
+        })
+    channel_id = json.loads(ch1.text)['channel_id']
+
+    id2 = requests.post(config.url + "auth/register/v2", 
+        json ={
+            'email': 'elephant@gmail.com',
+            'password': 'password',
+            'name_first': 'kelly',
+            'name_last': 'huang'
+            })
+    token2 = json.loads(id2.text)['token']
+    u_id1 = json.loads(id2.text)['auth_user_id']
+
+    id3 = requests.post(config.url + "auth/register/v2", 
+        json ={
+            'email': 'apple@gmail.com',
+            'password': 'password',
+            'name_first': 'hello',
+            'name_last': 'world'
+            })
+    u_id2 = json.loads(id3.text)['auth_user_id']
+
+    requests.post(config.url + 'channel/invite/v2', 
+        json ={
+            'token': token1,
+            'channel_id': channel_id,
+            'u_id': u_id1
+        })
+
+    resp1 = requests.post(config.url + "channel/addowner/v1", 
+        json = {
+            'token': token2,
+            'channel_id': channel_id,
+            'u_id': u_id2
+        })
+    assert resp1.status_code == 403
+
+def test_no_perm_not_owner_u_id_already_owner():
+    requests.delete(config.url + "clear/v1")
+    id1 = requests.post(config.url + "auth/register/v2", 
+        json = {
+            'email': 'abc@gmail.com',
+            'password': 'password',
+            'name_first': 'afirst',
+            'name_last': 'alast'
+        })
+
+    token1 = json.loads(id1.text)['token']
+
+    ch1 = requests.post(config.url + "channels/create/v2", 
+        json = {
+            'token': token1,
+            'name': '1531_CAMEl',
+            'is_public': True
+        })
+    channel_id = json.loads(ch1.text)['channel_id']
+
+    id2 = requests.post(config.url + "auth/register/v2", 
+        json ={
+            'email': 'elephant@gmail.com',
+            'password': 'password',
+            'name_first': 'kelly',
+            'name_last': 'huang'
+            })
+    token2 = json.loads(id2.text)['token']
+    u_id1 = json.loads(id2.text)['auth_user_id']
+
+    id3 = requests.post(config.url + "auth/register/v2", 
+        json ={
+            'email': 'apple@gmail.com',
+            'password': 'password',
+            'name_first': 'hello',
+            'name_last': 'world'
+            })
+    u_id2 = json.loads(id3.text)['auth_user_id']
+
+    requests.post(config.url + 'channel/invite/v2', 
+        json ={
+            'token': token1,
+            'channel_id': channel_id,
+            'u_id': u_id1
+        })
+    
+    requests.post(config.url + 'channel/invite/v2', 
+        json ={
+            'token': token1,
+            'channel_id': channel_id,
+            'u_id': u_id2
+        })
+
+    requests.post(config.url + "channel/addowner/v1", 
+        json = {
+            'token': token1,
+            'channel_id': channel_id,
+            'u_id': u_id1
+        })
+
+    requests.post(config.url + "channel/addowner/v1", 
+        json = {
+            'token': token1,
+            'channel_id': channel_id,
+            'u_id': u_id2
+        })
+
+    resp1 = requests.post(config.url + "channel/addowner/v1", 
+        json = {
+            'token': token2,
+            'channel_id': channel_id,
+            'u_id': u_id2
+        })
+        
+    assert resp1.status_code == 403
 
 def test_valid_addowner():
     requests.delete(config.url + "clear/v1")
@@ -315,6 +506,59 @@ def test_valid_addowner():
     requests.post(config.url + 'channel/invite/v2', 
         json ={
             'token': token,
+            'channel_id': channel_id,
+            'u_id': u_id
+        })
+
+    resp1 = requests.post(config.url + "channel/addowner/v1", 
+        json = {
+            'token': token,
+            'channel_id': channel_id,
+            'u_id': u_id
+        })
+
+    assert resp1.status_code == 200
+
+def test_addowner_valid_global():
+    requests.delete(config.url + "clear/v1")
+    id1 = requests.post(config.url + "auth/register/v2", 
+        json = {
+            'email': 'abc@gmail.com',
+            'password': 'password',
+            'name_first': 'afirst',
+            'name_last': 'alast'
+        })
+    token = json.loads(id1.text)['token']
+
+    id2 = requests.post(config.url + "auth/register/v2", 
+        json ={
+            'email': 'elephant@gmail.com',
+            'password': 'password',
+            'name_first': 'kelly',
+            'name_last': 'huang'
+            })
+
+    token2 = json.loads(id2.text)['token']
+    ch1 = requests.post(config.url + "channels/create/v2", 
+        json = {
+            'token': token2,
+            'name': '1531_CAMEl',
+            'is_public': True
+        })
+    channel_id = json.loads(ch1.text)['channel_id']
+
+    id3 = requests.post(config.url + "auth/register/v2", 
+        json ={
+            'email': 'hellokitty@gmail.com',
+            'password': 'password',
+            'name_first': 'hello',
+            'name_last': 'kitty'
+            })
+    u_id = json.loads(id3.text)['auth_user_id']
+
+    requests.post(config.url + 'channel/invite/v2', 
+        json ={
+            'token': token2,
             'channel_id': channel_id,
             'u_id': u_id
         })
