a88ad5b6ac8ee0b0c7965adca51ee2212c24b9e7
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-10-15 19:28:50 +1100

    fixed errors

--- src/dm.py
+++ src/dm.py
@@ -1,10 +1,10 @@
 from src.server_helper import decode_token
-from src.helper import check_valid_email, channels_user_details, check_valid_dm_id, check_valid_member_in_dm, channels_create_check_valid_user, get_handle, get_dm_info, user_info, check_creator, check_valid_dm, get_dm_dict
+from src.helper import check_valid_email, channels_user_details, check_valid_dm, check_valid_member_in_dm, channels_create_check_valid_user, get_handle, get_dm_info, user_info, check_creator, check_valid_dm, get_dm_dict
 from src.error import InputError, AccessError
 from src.data_store import DATASTORE, initial_object
 from src.auth import auth_register_v2
 
-def dm_details_v1(): 
+def dm_details_v1(token, dm_id): 
     '''
     Given an dm_id and token, the function provides
     basic details about the dm 
@@ -26,55 +26,57 @@ Return Value:
     '''
     store = DATASTORE.get() 
     auth_user_id = decode_token(token)
-    
-    if not check_valid_dm_id(dm_id): 
+
+
+
+    if not check_valid_dm(dm_id): 
         raise InputError("This dm_id does not refer to a valid DM")
 
     #not a valid user 
     if not check_valid_member_in_dm(dm_id, auth_user_id): 
-        raise AccessError("The user is not an authorised member of the DM)
+        raise AccessError("The user is not an authorised member of the DM")
 
-    for dms in initial_object['dms']: 
-        if dm_id == dms['dm_id']: 
+    for dm in initial_object['dms']: 
+        if dm_id == dm['dm_id']: 
             return { 
-                'name': dms['name']
-                'members': dms['members']
+                'name': dm['name'],
+                'members': dm['members']
             }
 
 
-def dm_messages_v1(): 
-    '''
-    Given a dm with dm_id that authorised user
-    is a member, return up to 50 messages between a index "start"
-     and "start + 50". Message with index 0 is the most recent message 
-    in the channel. returns a new index 'end'  which is 'start + 50'. 
-    returns -1 in end - no more messages to load. 
+# def dm_messages_v1(): 
+#     '''
+#     Given a dm with dm_id that authorised user
+#     is a member, return up to 50 messages between a index "start"
+#      and "start + 50". Message with index 0 is the most recent message 
+#     in the channel. returns a new index 'end'  which is 'start + 50'. 
+#     returns -1 in end - no more messages to load. 
 
-Arguments:
-    token - a user's unique token 
-    dm_id (int) - a user's unique dm id
-    start -  
-    ...
+# Arguments: 
+#     token - a user's unique token 
+#     dm_id (int) - a user's unique dm id
+#     start -  
+#     ...
 
-Exceptions:
-    InputError  - Occurs when the dm id is invalid 
-                    or the token is invalid.
-                - Start is greater then total number
-                of messages in channel 
-    AccessError - Occurs when the dm_id is not 
-                    an authorised member of the DM 
+# Exceptions:
+#     InputError  - Occurs when the dm id is invalid 
+#                     or the token is invalid.
+#                 - Start is greater then total number
+#                 of messages in channel 
+#     AccessError - Occurs when the dm_id is not 
+#                     an authorised member of the DM 
 
-Return Value:
-    Returns end 
-    '''
+# Return Value:
+#     Returns end 
+#     '''
 
-    #invalid dm_id
-    if not check_valid_dm_id(dm_id): 
-        raise InputError("This dm_id does not refer to a valid DM")
+#     #invalid dm_id
+#     if not check_valid_dm_id(dm_id): 
+#         raise InputError("This dm_id does not refer to a valid DM")
 
-    #not authorised  
-    if not check_valid_member_in_dm(dm_id, auth_user_id): 
-        raise AccessError("The user is not an authorised member of the DM)
+#     #not authorised  
+#     if not check_valid_member_in_dm(dm_id, auth_user_id): 
+#         raise AccessError("The user is not an authorised member of the DM")
 
 
 # for testing
@@ -107,10 +109,10 @@ def dm_create_v1(token, u_ids):
     # create a list that stores the handles of all the users given 
     # including creator
     handle_list = []
-    member_list = []
     creator_handle = get_handle(auth_user_id)
     handle_list.append(creator_handle)
     creator_info = user_info(auth_user_id)
+    member_list = [creator_info]
 
     for i in range(len(u_ids)):
         handle = get_handle(u_ids[i])
