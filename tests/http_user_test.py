07e0529af2db1edd9b290b7dba8dfbb7d359e4e4
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-10-17 22:12:42 +1100

    added fixture

--- tests/http_user_test.py
+++ tests/http_user_test.py
@@ -7,18 +7,33 @@ from src import config
 ##### user_profile_set_email tests #######
 ##########################################
 
-# Input Error when email entered is not a valid email
-def test_user_set_email_invalid_email():
-
-    requests.delete(config.url + "clear/v1", json={})
+@pytest.fixture
+def register_user1():
+    requests.delete(config.url + "clear/v1")
     user = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'abcdef@gmail.com',
+        'email': 'cat@gmail.com',
         'password': 'password',
         'name_first': 'anna',
         'name_last': 'lee'
     })
     user_data = user.json()
-    token = user_data['token']
+    return user_data
+
+@pytest.fixture
+def register_user2():
+    user2 = requests.post(config.url + "auth/register/v2", json ={
+        'email': 'elephant@gmail.com',
+        'password': 'password',
+        'name_first': 'sally',
+        'name_last': 'li'
+    })
+    user2_data = user2.json()
+    return user2_data
+
+# Input Error when email entered is not a valid email
+def test_user_set_email_invalid_email(register_user1):
+
+    token = register_user1['token']
 
     mail = requests.put(config.url + "user/profile/setemail/v1", json ={
         'token': token,
@@ -33,51 +48,27 @@ def test_user_set_email_invalid_email():
     assert mail1.status_code == 400
 
 # email address is already being used by another user
-def test_user_set_email_duplicate_email():
+def test_user_set_email_duplicate_email(register_user1, register_user2):
 
-    requests.delete(config.url + "clear/v1", json={})
-    user = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'cat@gmail.com',
-        'password': 'password',
-        'name_first': 'anna',
-        'name_last': 'lee'
-    })
-    user_data = user.json()
-    token = user_data['token']
-
-    user1 = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'elephant@gmail.com',
-        'password': 'password',
-        'name_first': 'sally',
-        'name_last': 'li'
-    })
-    user1_data = user1.json()
-    token1 = user1_data['token']
+    user1_token = register_user1['token']
+    user2_token = register_user2['token']
 
     mail = requests.put(config.url + "user/profile/setemail/v1", json ={
-        'token': token,
+        'token': user1_token,
         'email': 'elephant@gmail.com'
     })
 
     mail1 = requests.put(config.url + "user/profile/setemail/v1", json ={
-        'token': token1,
+        'token': user2_token,
         'email': 'cat@gmail.com'
     })
     assert mail.status_code == 400
     assert mail1.status_code == 400  
 
 # valid case
-def test_user_set_email_valid():
+def test_user_set_email_valid(register_user1):
 
-    requests.delete(config.url + "clear/v1", json={})
-    user = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'cat@gmail.com',
-        'password': 'password',
-        'name_first': 'anna',
-        'name_last': 'lee'
-    })
-    user_data = user.json()
-    token = user_data['token']
+    token = register_user1['token']
 
     mail = requests.put(config.url + "user/profile/setemail/v1", json ={
         'token': token,
@@ -91,17 +82,9 @@ def test_user_set_email_valid():
 ##########################################
 
 # length of handle_str is not between 3 and 20 characters inclusive
-def test_user_set_handle_invalid_length():
+def test_user_set_handle_invalid_length(register_user1):
 
-    requests.delete(config.url + "clear/v1", json={})
-    user = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'abcdef@gmail.com',
-        'password': 'password',
-        'name_first': 'anna',
-        'name_last': 'lee'
-    })
-    user_data = user.json()
-    token = user_data['token']
+    token = register_user1['token']
 
     handle = requests.put(config.url + "user/profile/sethandle/v1", json ={
         'token': token,
@@ -116,54 +99,37 @@ def test_user_set_handle_invalid_length():
     assert handle1.status_code == 400
 
 # handle_str contains characters that are not alphanumeric
-def test_user_set_handle_non_alphanumeric():
+def test_user_set_handle_non_alphanumeric(register_user1):
     
-    requests.delete(config.url + "clear/v1", json={})
-    user = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'abcdef@gmail.com',
-        'password': 'password',
-        'name_first': 'anna',
-        'name_last': 'lee'
-    })
-    user_data = user.json()
-    token = user_data['token']
+    token = register_user1['token']
+
     handle = requests.put(config.url + "user/profile/sethandle/v1", json ={
         'token': token,
         'handle_str': '___ad31__++'
     })
     assert handle.status_code == 400
 
-def test_user_set_handle():
-
-    requests.delete(config.url + "clear/v1", json={})
-    user = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'abcdef@gmail.com',
-        'password': 'password',
-        'name_first': 'anna',
-        'name_last': 'lee'
+    handle = requests.put(config.url + "user/profile/sethandle/v1", json ={
+        'token': token,
+        'handle_str': '___ad31__:  '
     })
-    user_data = user.json()
-    token = user_data['token']
+    assert handle.status_code == 400
 
-    user1 = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'elephant@gmail.com',
-        'password': 'password',
-        'name_first': 'sally',
-        'name_last': 'li'
-    })
-    user1_data = user1.json()
-    token1 = user1_data['token']
+def test_user_set_handle(register_user1, register_user2):
+
+    user1_token = register_user1['token']
+    user2_token = register_user2['token']
 
     # valid case
     handle = requests.put(config.url + "user/profile/sethandle/v1", json ={
-        'token': token,
+        'token': user1_token,
         'handle_str': 'anna'
     })
     assert handle.status_code == 200
 
     # the handle is already used by another user
     handle = requests.put(config.url + "user/profile/sethandle/v1", json ={
-        'token': token1,
+        'token': user2_token,
         'handle_str': 'anna'
     })
     assert handle.status_code == 400
