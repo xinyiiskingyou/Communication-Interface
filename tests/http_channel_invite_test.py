521bf3bb2f2d691e2d23dd3efb2a71b28f4b7dc8
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-10-17 21:44:46 +1100

    added fixture

--- tests/http_channel_invite_test.py
+++ tests/http_channel_invite_test.py
@@ -7,8 +7,8 @@ from src import config
 ########## channel_invite tests ##########
 ##########################################
 
-# Invalid u_id
-def test_invite_invalid_u_id():
+@pytest.fixture
+def register_user():
 
     requests.delete(config.url + "clear/v1")
     user = requests.post(config.url + "auth/register/v2", json ={
@@ -18,150 +18,115 @@ def test_invite_invalid_u_id():
         'name_last': 'lee'
     })
     user_data = user.json()
-    token = user_data['token']
+    return user_data
+
+@pytest.fixture
+def register_user1():
+
+    user1 = requests.post(config.url + "auth/register/v2", json ={
+        'email': 'abcd@gmail.com',
+        'password': 'password',
+        'name_first': 'sally',
+        'name_last': 'li'
+    })
+    user1_data = user1.json()
+    return user1_data
+
+@pytest.fixture
+def create_channel(register_user):
 
     channel = requests.post(config.url + "channels/create/v2", json ={
-        'token': token,
+        'token': register_user['token'],
         'name': 'anna',
         'is_public': True
     })
+    channel_data = channel.json()
+    return channel_data
 
-    channel_id1 = json.loads(channel.text)['channel_id']
+# Invalid u_id
+def test_invite_invalid_u_id(register_user, register_user1, create_channel):
 
-    invite = requests.post(config.url + 'channel/invite/v2', json ={
+    token = register_user['token']
+    channel = create_channel['channel_id']
+
+    invite = requests.post(config.url + "channel/invite/v2", json ={
         'token': token,
-        'channel_id': channel_id1,
+        'channel_id': channel,
         'u_id': -16
     })
     assert invite.status_code == 400
 
+    # Access error channel_id is valid and authorised user is not a member of the channel
+    # and the u_id is invalid
+    user2_token = register_user1['token']
+    invite = requests.post(config.url + "channel/invite/v2", json ={
+        'token': user2_token,
+        'channel_id': channel,
+        'u_id': -16
+    })
+    assert invite.status_code == 403
+
 # Invalid channel_id
-def test_invite_invalid_channel_id():
-    requests.delete(config.url + "clear/v1")
+def test_invite_invalid_channel_id(register_user, register_user1):
 
-    user = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'abcde@gmail.com',
-        'password': 'password',
-        'name_first': 'anna',
-        'name_last': 'li'
-    })
-    user_data = user.json()
-    token = user_data['token']
+    user_token = register_user['token']
+    user1_token = register_user1['token']
+    u_id = register_user1['auth_user_id']
 
-    user1 = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'abcd@gmail.com',
-        'password': 'password',
-        'name_first': 'sally',
-        'name_last': 'li'
-    })
-    user1_data = user1.json()
-    u_id = user1_data['auth_user_id']
-
-    invite = requests.post(config.url + 'channel/invite/v2', json ={
-        'token': token,
+    invite = requests.post(config.url + "channel/invite/v2", json ={
+        'token': user_token,
         'channel_id': -16,
         'u_id': u_id
     })
     assert invite.status_code == 400
-
-def test_invite_already_member():
+    
+def test_invite_already_member(register_user, create_channel, register_user1):
     requests.delete(config.url + "clear/v1")
 
     # create a user that has channel
-    user = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'abcd@gmail.com',
-        'password': 'password',
-        'name_first': 'anna',
-        'name_last': 'li'
-    })
-    user_data = user.json()
-    token = user_data['token']
-
-    channel = requests.post(config.url + "channels/create/v2", json = {
-        'token': token,
-        'name': 'anna',
-        'is_public': True
-    })
-    channel_id = json.loads(channel.text)['channel_id']
+    user1_token = register_user['token']
+    user1_id = register_user['auth_user_id']
+    channel_id = create_channel['channel_id']
 
     # create 2 users that don't have channels
-    user1 = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'abc@gmail.com',
-        'password': 'password',
-        'name_first': 'sally',
-        'name_last': 'li'
-    })
-    user1_data = user1.json()
-    token1 = user1_data['token']
-    u_id = user1_data['auth_user_id']
+    user2_id = register_user1['auth_user_id']
 
-    user2 = requests.post(config.url + "auth/register/v2", json ={
+    user3 = requests.post(config.url + "auth/register/v2", json ={
         'email': 'elephant@gmail.com',
         'password': 'password',
         'name_first': 'kelly',
         'name_last': 'huang'
     })
-    user2_data = user2.json()
-    u_id2 = user2_data['auth_user_id']
+    user3_data = user3.json()
+    user3_token = user3_data['token']
 
     # test error when channel_id is valid but authorised user is not a member of the channel
-    channel_invite = requests.post(config.url + 'channel/invite/v2', json ={
-        'token': token1,
+    channel_invite = requests.post(config.url + "channel/invite/v2", json ={
+        'token': user3_token,
         'channel_id': channel_id,
-        'u_id': u_id2
+        'u_id': user2_id
     })
-
     assert channel_invite.status_code == 403
 
-    # now let user1 join the channel
-    join = requests.post(config.url + 'channel/invite/v2', json ={
-        'token': token,
-        'channel_id': channel_id,
-        'u_id': u_id
-    })
-    assert join.status_code == 200
-
     # test error when u_id refers to a user who is already a member of the channel
-    invite = requests.post(config.url + 'channel/invite/v2', json ={
-        'token': token,
+    invite = requests.post(config.url + "channel/invite/v2", json ={
+        'token': user1_token,
         'channel_id': channel_id,
-        'u_id': u_id
+        'u_id': user1_id
     })
     assert invite.status_code == 400
 
-def test_valid_channel_invite():
-    requests.delete(config.url + "clear/v1")
+def test_valid_channel_invite(register_user, create_channel, register_user1):
 
-    user = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'abcd@gmail.com',
-        'password': 'password',
-        'name_first': 'anna',
-        'name_last': 'li'
-    })
-    user_data = user.json()
-    token = user_data['token']
+    token = register_user['token']
+    channel_id = create_channel['channel_id']
+    # invite an user that is not a member of the channel
+    u_id = register_user1['auth_user_id']
 
-    channel = requests.post(config.url + "channels/create/v2", json ={
-        'token': token,
-        'name': 'anna',
-        'is_public': True
-    })
-    channel_id = json.loads(channel.text)['channel_id']
-
-    user2 = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'elephant@gmail.com',
-        'password': 'password',
-        'name_first': 'kelly',
-        'name_last': 'huang'
-    })
-    user2_data = user2.json()
-    u_id = user2_data['auth_user_id']
-
-    invite = requests.post(config.url + 'channel/invite/v2', json ={
+    invite = requests.post(config.url + "channel/invite/v2", json ={
         'token': token,
         'channel_id': channel_id,
         'u_id': u_id
     })
 
     assert invite.status_code == 200
-
