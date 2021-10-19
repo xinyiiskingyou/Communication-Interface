f3ceca5a687486bd595f966a70b7cf5583eb77a4
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-10-19 12:10:13 +1100

    fixed the coverage to 100%

--- src/admin.py
+++ src/admin.py
@@ -24,24 +24,19 @@ def admin_user_remove_v1(token, u_id):
         raise AccessError(description='User is not valid')
 
     auth_user_id = decode_token(token)
+
+    # the authorised user is not a global owner
+    if not check_permision_id(auth_user_id):
+        raise AccessError(description='The authorised user is not a global owner')
+
     # u_id does not refer to a valid user
     if not channels_create_check_valid_user(u_id):
-        # u_id is invalid and authorised user is not a global owner
-        if not check_permision_id(auth_user_id):
-            raise AccessError(description='The authorised user is not a global owner')
         raise InputError(description='The u_id does not refer to a valid user')
 
     # u_id refers to a user who is the only global owner
     user = check_number_of_owners(u_id)
     if user == 0:
-        # and the authorised user is not a global owner
-        if not check_permision_id(auth_user_id):
-            raise AccessError(description='The authorised user is not a global owner')
         raise InputError(description='The u_id refers to a user who is the only global owner')
-
-    # the authorised user is not a global owner
-    if not check_permision_id(auth_user_id):
-        raise AccessError(description='The authorised user is not a global owner')
 
     # remove users from channel
     for channel in initial_object['channels']:
@@ -110,31 +105,23 @@ def admin_userpermission_change_v1(token, u_id, permission_id):
 
     auth_user_id = decode_token(token)
 
+    # the authorised user is not a global owner
+    if not check_permision_id(auth_user_id):
+        raise AccessError(description='The authorised user is not a global owner')
+
     # u_id does not refer to a valid user
     if not isinstance(u_id, int) or not channels_create_check_valid_user(u_id):
-        # if u_id is invalid the authorised user is not a global owner
-        if not check_permision_id(auth_user_id):
-            raise AccessError(description='The authorised user is not a global owner')
         raise InputError(description='The u_id does not refer to a valid user')
     
     # u_id refers to a user who is the only global owner and they are being demoted to a user
     owner = check_number_of_owners(u_id)
     if owner == 0 and not check_permission(u_id, permission_id):
-        # if input error and access error raises at the same time 
-        if not check_permision_id(auth_user_id):
-            raise AccessError(description='The authorised user is not a global owner')
         raise InputError(description='U_id refers to the only global owner and they are being demoted')
 
     # permission_id is invalid
     if permission_id < 1 or permission_id > 2:
-        # if permission id is invalid and the authorised user is not a global owner
-        if not check_permision_id(auth_user_id):
-            raise AccessError(description='The authorised user is not a global owner')
         raise InputError(description='The permission_id is invalid')
 
-    # the authorised user is not a global owner
-    if not check_permision_id(auth_user_id):
-        raise AccessError(description='The authorised user is not a global owner')
 
     user = channels_user_details(u_id)
     user['permission_id'] = permission_id
