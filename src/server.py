300d29a711668fcf601124b10196d99b8d7a2842
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-10-23 02:42:36 +1100

    remove extra import

--- src/server.py
+++ src/server.py
@@ -7,17 +7,14 @@ from flask_cors import CORS
 from src.error import InputError
 from src import config
 
+from src.admin import admin_user_remove_v1, admin_userpermission_change_v1
 from src.auth import auth_register_v2, auth_login_v2, auth_logout_v1
 from src.channels import channels_listall_v2,channels_create_v2, channels_list_v2
 from src.channel import channel_join_v2, channel_details_v2, channel_invite_v2, channel_leave_v1
 from src.channel import channel_removeowner_v1, channel_addowner_v1, channel_messages_v2
-from src.user import users_all_v1, user_profile_v1, user_profile_setname_v1, user_profile_setemail_v1, user_profile_sethandle_v1 
-from src.message import message_send_v1, message_edit_v1, message_remove_v1
-from src.dm import dm_create_v1, dm_list_v1, dm_remove_v1, dm_details_v1
-from src.admin import admin_user_remove_v1, admin_userpermission_change_v1
-from src.user import user_profile_sethandle_v1, user_profile_setemail_v1, user_profile_setname_v1, user_profile_v1, users_all_v1
-from src.message import message_send_v1
 from src.dm import dm_create_v1, dm_list_v1, dm_remove_v1, dm_details_v1, message_senddm_v1, dm_messages_v1, dm_leave_v1
+from src.message import message_send_v1, message_edit_v1, message_remove_v1
+from src.user import user_profile_sethandle_v1, user_profile_setemail_v1, user_profile_setname_v1, user_profile_v1, users_all_v1
 from src.other import clear_v1
 
 def quit_gracefully(*args):
