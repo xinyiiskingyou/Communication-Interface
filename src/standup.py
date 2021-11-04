3d54bde2b1a5c48c98c1a118367c2d9f8f19d465
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-11-04 23:26:27 +1100

    added error checking

--- src/standup.py
+++ src/standup.py
@@ -37,6 +37,8 @@ def standup_start_v1(token, channel_id, length):
 
 
 
+
+
     return time_finish 
 
 
@@ -57,6 +59,17 @@ def standup_active_v1(token, channel_id):
     Return Value:
         Returns if the standup is active and the time is finishes(finished)
     '''  
+    if not valid_user(token):
+        raise AccessError(description='User is not valid')
+
+    auth_user_id = decode_token(token)
+
+    # Input error when channel_id does not refer to a valid channel
+    if not isinstance(channel_id, int) or not check_valid_channel_id(channel_id):
+        raise InputError(description = 'Channel_id does not refer to a valid channel')
+
+
+
     return {is_active, time_finish}
 
 
@@ -78,4 +91,14 @@ def standup_send_v1(token, channel_id, message):
     Return Value:
         Returns if the standup is active and the time is finishes(finished)
     '''  
+    if not valid_user(token):
+        raise AccessError(description='User is not valid')
+
+    auth_user_id = decode_token(token)
+
+    # Input error when channel_id does not refer to a valid channel
+    if not isinstance(channel_id, int) or not check_valid_channel_id(channel_id):
+        raise InputError(description = 'Channel_id does not refer to a valid channel')
+
+
     return {}
