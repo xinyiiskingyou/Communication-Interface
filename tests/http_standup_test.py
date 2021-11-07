44b75b039410ec898778c18c8ea6d20e5c0af579
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-11-07 22:35:33 +1100

    only has error tests atm

--- tests/http_standup_test.py
+++ tests/http_standup_test.py
@@ -3,65 +3,47 @@ import json
 from src.error import AccessError, InputError
 import requests
 from src import config
-##import a fixture file(probably xinyi made it) 
+import fixture 
 
-@pytest.fixture
-def register_user():
-
-    requests.delete(config.url + "clear/v1")
-    user = requests.post(config.url + "auth/register/v2", json ={
-        'email': 'abcdef@gmail.com',
-        'password': 'password',
-        'name_first': 'anna',
-        'name_last': 'lee'
-    })
-    user_data = user.json()
-    return user_data
-
-@pytest.fixture
-def create_public_channel(register_user):
-
-    channel = requests.post(config.url + "channels/create/v2", json ={
-        'token': register_user['token'],
-        'name': 'channel1',
-        'is_public': True
-    })
-    channel_data = channel.json()
-    return channel_data
+####ERROR TESTS FOR ALL STANDUP FUNCTIONS### 
 
 
+##STANDUP START### 
 def test_standup_invalid_channel_id(register_user):
- 
     token1 = register_user['token']
 
     # Test invalid channel_id
     resp1 = requests.get(config.url + "standup/start/v1", params = {
         'token': token1,
-        'channel_id': -1
+        'channel_id': -1,
+        'length': 1
     })
 
     resp2 = requests.get(config.url + "standup/start/v1", params = {
         'token': token1,
-        'channel_id': 0
+        'channel_id': 0,
+        'length': 1
     })
 
     resp3 = requests.get(config.url + "standup/start/v1", params = {
         'token': token1,
-        'channel_id': 256
+        'channel_id': 256,
+        'length': 1
     })
 
-    assert resp1.status_code == 400
-    assert resp2.status_code == 400
-    assert resp3.status_code == 400
+    assert resp1.status_code == INPUTERROR
+    assert resp2.status_code == INPUTERROR
+    assert resp3.status_code == INPUTERROR
 
     requests.post(config.url + "auth/logout/v1", json = {
         'token': token1
     })
     resp1 = requests.get(config.url + "standup/start/v1", params ={
         'token': token1,
-        'channel_id': 256
+        'channel_id': 256,
+        'length': 1
     })
-    assert resp1.status_code == 403
+    assert resp1.status_code == INPUTERROR
 
 
 def test_standup_invalid_token(register_user, create_public_channel):
@@ -74,6 +56,75 @@ def test_standup_invalid_token(register_user, create_public_channel):
 
     resp1 = requests.get(config.url + "standup/start/v1", params ={
         'token': token,
-        'channel_id': channel
+        'channel_id': channel,
+        'length': 1
     })
-    assert resp1.status_code == 403
+    assert resp1.status_code == INPUTERROR
+
+def test_standup_negative_length(register_user, create_public_channel): 
+    token = register_user['token']
+    channel = create_public_channel['channel_id']
+    resp1 = requests.get(config.url + "standup/start/v1", params ={
+        'token': token,
+        'channel_id': channel,
+        'length': -1
+    })
+    assert resp1.status_code == INPUTERROR
+
+
+
+###STANDUP ACTIVE### 
+def test_standup_active_invalid_channel(): 
+    token1 = register_user['token']
+
+    # Test invalid channel_id
+    resp1 = requests.get(config.url + "standup/active/v1", params = {
+        'token': token1,
+        'channel_id': -1,
+        'length': 1
+    })
+
+    resp2 = requests.get(config.url + "standup/active/v1", params = {
+        'token': token1,
+        'channel_id': 0,
+        'length': 1
+    })
+
+    resp3 = requests.get(config.url + "standup/active/v1", params = {
+        'token': token1,
+        'channel_id': 256,
+        'length': 1
+    })
+
+    assert resp1.status_code == INPUTERROR
+    assert resp2.status_code == INPUTERROR
+    assert resp3.status_code == INPUTERROR
+
+    requests.post(config.url + "auth/logout/v1", json = {
+        'token': token1
+    })
+    resp1 = requests.get(config.url + "standup/active/v1", params ={
+        'token': token1,
+        'channel_id': 256,
+        'length': 1
+    })
+    assert resp1.status_code == INPUTERROR
+
+
+def test_standup_active_invalid_token(register_user, create_public_channel):
+
+    token = register_user['token']
+    channel = create_public_channel['channel_id']
+    requests.post(config.url + "auth/logout/v1", json = {
+        'token': token
+    })
+
+    resp1 = requests.get(config.url + "standup/active/v1", params ={
+        'token': token,
+        'channel_id': channel,
+        'length': 1
+    })
+    assert resp1.status_code == INPUTERROR
+
+
+    
