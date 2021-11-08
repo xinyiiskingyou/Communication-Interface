ea665ce5f7d325653cedb3e9b47f08c704a7010f
Ashley.wong <z5362896@ad.unsw.edu.au>
2021-11-08 23:19:46 +1100

    hopefully works - logic makes sense - haven't tested yet

--- src/standup.py
+++ src/standup.py
@@ -46,16 +46,12 @@ def standup_start_v1(token, channel_id, length):
      for channel in get_data()['channels']:
         if channel['channel_id'] == channel_id: 
             time_finish = datetime.utcnow().replace(tzinfo=timezone.utc).timestamp() + length
-            # channel['standup']['u_id_start'] = auth_user_id
-            t1 = threading.Thread(target = standup_send_v1, args=[token, channel_id, message])
-            # t2 = threading.Thread(target = thread_helper, args = [token, channel_id, message])
+            t1 = threading.Thread(target = thread_helper, arg[length, auth_user_id, channel_id])
+            t1.daemon = True
             channel['standup']['standup_active'] = True
             t1.start()
-            time.sleep(length)
-            t1.end()
-            channel['standup']['standup_active'] = False
             channel['standup']['time_finished'] = time_finish
-            return time_finish
+            return {'time_finish': time_finish
 
             
 
@@ -127,79 +123,20 @@ def standup_send_v1(token, channel_id, message):
 
     if standup_active_v1(token, channel_id) == False: 
         raise InputError 
-    queue = []
-     for channel in get_data()['channels']:
-        if channel['channel_id'] == channel_id:
-            if : 
-                standup_message_queue(token, message, queue)
-            else: 
-                standup_message = queue_compile(queue, message)
-                channel['standup']['standup_message'] = standup_message
-                ####figure out how to actually sned the message (like message send) 
-                get_data()['messages'].insert(0, message_details_messages)
-                save()
+        queue = []
+        for channel in get_data()['channels']:
+            if channel['channel_id'] == channel_id:
+                if channel['standup']['standup_active'] == True: 
+                    standup_message_queue(token, message, channel_id)
     return {}
 
 
-#### scrap code ### a
+#### scrap code ### 
 
-    # t1 = threading.Thread(target=standup_start_v1)
-# t2 = threading.Thread(target=standup_start_v1)
-    #     print(f'starting for {length}')
-#     bool time_finish = False
-#     time.sleep(length)
-#     print("end of standup")
-#     time_finish = True
-#     return time_finish 
-
-##while the standup start is paused - process all the messages and queue in send? 
-# def thread_helper(length, message):
-#     start_time = time.time() 
-
-#     while True: 
-#         currtime = time.tine()
-#         time_past = currtime - start_time
-
-#         if (currtime > length): 
-#             break
-    ##message id
-    # message_id = (len(get_data()['messages']) * 2) + 1
-    #  # Append dictionary of message details into initial_objects['channels']['messages']
-    #  ##get the channel ... tho it i should change the helper function parameters for this 
-    #  ##and then append it the message to a list????? 
-    #  queue = []
-    # for channel in get_data()['channels']:
-    #     if channel['channel_id'] == channel_id:
-    #         channel['messages'].insert(0, message_details_channels)
-    #         save()
-
-    # message_details_messages = {
-    #     'message_id': message_id,
-    #     'u_id': auth_user_id, 
-    #     'message': message,
-    #     'time_created': time_created,
-    #     'channel_id': channel_id,
-    #     'reacts':[reacts_details],
-    #     'is_pinned': bool(is_pinned)
-    # }
-
-
-##might help with the last bit of creation
-# def message_create_for_standup(channel_id, u_id, message):
-#     user = u_id_check(u_id)
-#     channel = channel_check(channel_id)
-#     final_string = channel["standup"]["standup_message"]
-#     return_string = user['handle_str'] + ":" + message + '\n'
-#     channel["standup"]["standup_message"] = final_string + return_string
-
-
-##so basically thread to standup send - append each message and then when the time runs out 
-##compile and 
-
-
+ 
 ##helper function for storing the messages that aren't the last one## 
 
-def standup_message_queue(token, message, queue):
+def standup_message_queue(token, message, channel_id):
     if not valid_user(token):
         raise AccessError(description='User is not valid')
 
@@ -208,15 +145,14 @@ def standup_message_queue(token, message, queue):
         if user['u_id'] = auth_user_id: 
             newuser = user['handle_str'] 
     standup_mess = newuser + ":" + message + '\n'
-    queue.append(standup_mess)
+    for channel in get_data()['channels']: 
+        if channel['channel_id'] == channel_id: 
+            channel['standup']['message'] = channel['standup']['message'] + message
 
-def queue_compile(queue, message): 
-    for user in get_data()['users']: 
-        if user['u_id'] = auth_user_id: 
-            newuser = user['handle_str']
-    new_message =  newuser + ":" + message + '\n'
-    for x in queue:
-        new_message = new_message + x
-    return new_message
-        
-# not channel['standup']['time_finished'] - datetime.utcnow().replace(tzinfo=timezone.utc).timestamp() > 0
+##pause and then after it's done grab the messages from standup_messages and send them 
+def thread_helper(length, token, channel_id):
+    time.sleep(length)
+    auth_user_id = decode_token(token)
+    channel['standup']['standup_active'] = False
+    s_message = channel['standup']['standup_message']
+    message_send_v1(token, channel_id, s_message)
