e58a8a9172a723cf9a74844e340aee7ef2086638
Xinyi Lin <z5364823@cse.unsw.edu.au>
2021-11-09 20:36:31 +1100

    added a test when the standup has activated

--- tests/http_standup_start_test.py
+++ tests/http_standup_start_test.py
@@ -93,6 +93,25 @@ def test_standup_start_invalid_length(global_owner, create_channel, register_use
     })
     assert resp1.status_code == ACCESSERROR
 
+def test_standup_already_active(global_owner, create_channel):
+
+    channel_id = create_channel['channel_id']
+    token1 = global_owner['token']
+
+    resp1 = requests.post(config.url + "standup/start/v1", json ={
+        'token': token1,
+        'channel_id': channel_id,
+        'length': 10
+    })
+    assert resp1.status_code == VALID
+
+    resp1 = requests.post(config.url + "standup/start/v1", json ={
+        'token': token1,
+        'channel_id': channel_id,
+        'length': 1
+    })
+    assert resp1.status_code == INPUTERROR
+
 # Access error: user is not a member of channel
 def test_standup_user_not_member(global_owner, register_user2, create_channel):
 
